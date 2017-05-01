6edc67cae330f90cc28743eaea6a78ae542ad780
Martin Taillefer <mtail@google.com>
2017-05-01 12:24:26 -0700

    A bit more work on search integration.

---
+++ js/search.js
@@ -0,0 +1,9 @@
+(function() {
+    var cx = '013699703217164175118:veyyqmfmpj4';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+})();
