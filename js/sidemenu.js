ee5fb64ca3e861dc60cc564b325769747dfe20e7
gauliang <gauliang@outlook.com>
2017-05-23 11:25:58 -0400

    fixed leftnav not responding on mobile (#309)

--- js/sidemenu.js
+++ js/sidemenu.js
@@ -4,7 +4,7 @@
     var sidebar = $("#sidebar-container");
     var content = $("#content-container");
     var tab = $("#tab-container");
-    var width = $(document). width();
+    var hasExpandedNavOnSmallScreen = false;
 
     function setSidebar() {
       // set sidebar height, breakpoint at iPad in landscape
@@ -13,35 +13,30 @@
     
       // 992 is the default breakpoint set in Bootstrap 3 for col-md
       var width = $(document).width();
-      if (width < 992) {
+      if (width < 992 && !hasExpandedNavOnSmallScreen) {
         sidebar.hide();
         sidebarOpen = false;
       }
     }
 
     $(document).on('click', '#sidebar-tab', function() {
+      hasExpandedNavOnSmallScreen = true;
       $(this).toggleClass('glyphicon-chevron-right');
       $(this).toggleClass('glyphicon-chevron-left');
       sidebar.toggle(250);
       sidebarOpen = !sidebarOpen;
 
       if (!sidebarOpen) {
-        tab.removeClass("col-xs-1");
-        tab.removeClass("tab-neg-margin");
-        tab.removeClass("pull-left");
+        tab.removeClass("col-xs-1 tab-neg-margin pull-left");
 
-        content.removeClass("thin-left-border");
-        content.removeClass("col-sm-9");
+        content.removeClass("thin-left-border col-sm-9");
         content.addClass("col-sm-11");
       }
       else {
-        tab.addClass("col-xs-1");
-        tab.addClass("tab-neg-margin");
-        tab.addClass("pull-left");
+        tab.addClass("col-xs-1 tab-neg-margin pull-left");
         
         content.removeClass("col-sm-11");
-        content.addClass("thin-left-border");
-        content.addClass("col-sm-9");
+        content.addClass("thin-left-border col-sm-9");
       }
     });
 
