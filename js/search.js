0dc710e65f623dcb8dfbd9a113e4caca134ab484
Martin Taillefer <geeknoid@users.noreply.github.com>
2018-02-06 20:40:21 -0800

    Update for archiving (#847)

--- js/search.js
+++ js/search.js
@@ -1,6 +1,10 @@
+---
+---
+{% include home.html %}
+
 (function ($) {
     function doSearch() {
-        var url = '/search/?q=' + document.getElementsByName('q')[0].value;
+        var url = '{{home}}/search/?q=' + document.getElementsByName('q')[0].value;
         window.location.assign(url);
     }
 
