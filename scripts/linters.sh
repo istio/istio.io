a60a95be0d0f0fd3c6d49815a30e43f14d282cbb
gauliang <gauliang@outlook.com>
2018-04-05 10:31:50 -0700

    Make spell checking and style checking part of our doc checkin gate. (#1154)

--- scripts/linters.sh
+++ scripts/linters.sh
@@ -1,3 +1,10 @@
+#! /bin/sh
+# This script will spell check all markdown files in the repo,
+# while style check those files, and finally will proof the
+# final HTML looking for broken links and other common errors.
+set -x
+set -e
+
 mdspell --en-us --ignore-acronyms --ignore-numbers --no-suggestions --report *.md */*.md */*/*.md */*/*/*.md */*/*/*/*.md
 mdl --ignore-front-matter --style mdl_style.rb .
-rake test
+bundle exec rake test
