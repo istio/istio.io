cf7591929f4d574958365f15538f3f5db2668262
Martin Taillefer <geeknoid@users.noreply.github.com>
2018-05-01 11:11:41 -0700

    Introduce support for new fangled PRE blocks. (#1224)
    Instead of having to have two PRE blocks, one for commands and one for the output,
    we can now have a single PRE block and we take care of rendering things to show the
    command vs. the output. The Copy button on such a thing only copy the command, and not
    the output.
    
    We now also show a $ on command-lines, but the Copy button doesn't copy that and knows to just
    copy the usable part of the command-line.

--- js/misc.js
+++ js/misc.js
@@ -146,8 +146,27 @@ function handleDOMLoaded() {
             }
 
             var copyCode = new Clipboard('button.copy', {
-                target: function (trigger) {
-                    return trigger.previousElementSibling;
+                text: function (trigger) {
+                    var commands = trigger.previousElementSibling.getElementsByClassName("command");
+                    if ((commands != undefined) && (commands.length > 0)) {
+                        var lines = commands[0].innerText.split("\n");
+                        var cmd = "";
+                        for (var i = 0; i < lines.length; i++) {
+                            if (lines[i].startsWith("$ ")) {
+                                lines[i] = lines[i].substring(2);
+                            }
+
+                            if (cmd != "") {
+                                cmd = cmd + "\n";
+                            }
+
+                            cmd = cmd + lines[i];
+                        }
+
+                        return cmd;
+                    }
+
+                    return trigger.previousElementSibling.innerText;
                 }
             });
 
@@ -178,8 +197,57 @@ function handleDOMLoaded() {
         function applySyntaxColoring() {
             var pre = document.getElementsByTagName('PRE');
             for (var i = 0; i < pre.length; i++) {
-                if (!pre[i].hasAttribute("data-src")) {
-                    Prism.highlightElement(pre[i].firstChild, false)
+                var code = pre[i].firstChild;
+
+                if (code.classList.contains("language-command")) {
+                    var text = code.innerText;
+                    var lines = text.split("\n");
+
+                    var bottom = false;
+                    var cmd = "";
+                    var output = "";
+                    var escape = false;
+                    for (var j = 0; j < lines.length; j++) {
+                        if (bottom) {
+                            output = output + "\n" + lines[j]
+                        } else {
+                            if (lines[j].startsWith("$ ")) {
+                                // line is definitely a command
+                            } else if (escape) {
+                                // continuation
+                            } else {
+                                bottom = true;
+                                output = lines[j];
+                                continue;
+                            }
+
+                            escape = (lines[j].endsWith("\\"));
+
+                            if (cmd != "") {
+                                cmd = cmd + "\n";
+                            }
+                            cmd = cmd + lines[j]
+                        }
+                    }
+
+                    var colored = Prism.highlight(cmd, Prism.languages["bash"], "bash");
+                    var html = "<div class='command'>" + colored + "</div>";
+
+                    if (output != "") {
+                        html = html + "<div class='output'>" + output + "</div>";
+                    }
+
+                    code.innerHTML = html;
+                    code.classList.remove("language-command");
+                    code.classList.add("command-output");
+                } else {
+                    Prism.highlightElement(code, false);
+                    var code = pre[i].firstChild;
+                    var div = document.createElement("DIV");
+                    div.className = "code-plain";
+                    parent = code.parentElement;
+                    parent.insertBefore(div, code);
+                    div.appendChild(code);
                 }
             }
         }
@@ -238,7 +306,6 @@ function handleDOMLoaded() {
             function fetchFile(elem, url) {
                 fetch(url).then(response => response.text()).then(data => {
                     elem.firstChild.innerText = data;
-                    Prism.highlightElement(elem.firstChild, false)
                 });
             }
 
