a6085e1b38d11e225ca574960ac1d3ac6b5f237a
mtail <mtail@google.com>
2018-06-14 17:20:19 -0700

    Switch site operations to a docker image to simplify use and automation.

---
+++ scripts/build_site.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+sass src/sass/light_theme_archive.scss light_theme_archive.css -s compressed
+sass src/sass/light_theme_normal.scss light_theme_normal.css -s compressed
+sass src/sass/light_theme_preliminary.scss light_theme_preliminary.css -s compressed
+sass src/sass/dark_theme_archive.scss dark_theme_archive.css -s compressed
+sass src/sass/dark_theme_normal.scss dark_theme_normal.css -s compressed
+sass src/sass/dark_theme_preliminary.scss dark_theme_preliminary.css -s compressed
+mv light_theme* static/css
+mv dark_theme* static/css
+uglifyjs src/js/misc.js src/js/prism.js --mangle --compress -o static/js/all.min.js --source-map
+uglifyjs src/js/styleSwitcher.js --mangle --compress -o static/js/styleSwitcher.min.js --source-map
