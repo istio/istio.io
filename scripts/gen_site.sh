39105c2468ca9ab8629f938ef63fafa4270b7c70
Martin Taillefer <geeknoid@users.noreply.github.com>
2018-06-15 17:02:08 -0700

    Update Makefile for simplified Netlify scheme. (#1525)

--- scripts/gen_site.sh
+++ scripts/gen_site.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
+set -e
 
 hugo --baseURL "$1"
 html-minifier --input-dir public --output-dir public --file-ext html --collapse-whitespace --minify-js --minify-css --sort-attributes --sort-class-name --remove-attribute-quotes --remove-comments
--- scripts/lint_site.sh
+++ scripts/lint_site.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
+set -e
 
 mdspell --en-us --ignore-acronyms --ignore-numbers --no-suggestions --report content/*.md content/*/*.md content/*/*/*.md content/*/*/*/*.md content/*/*/*/*/*.md content/*/*/*/*/*/*.md content/*/*/*/*/*/*/*.md
+#mdspell --zh-cn --ignore-acronyms --ignore-numbers --no-suggestions --report content_zh/*.md content_zh/*/*.md content_zh/*/*/*.md content_zh/*/*/*/*.md content_zh/*/*/*/*/*.md content_zh/*/*/*/*/*/*.md content_zh/*/*/*/*/*/*/*.md
 mdl --ignore-front-matter --style mdl_style.rb .
 htmlproofer ./public --check-html --assume-extension --timeframe 2d --storage-dir .htmlproofer --url-ignore "/localhost/,/github.com/istio/istio.github.io/edit/master/,/github.com/istio/istio/issues/new/choose/"
