5dce472cfd8d079c34f154ab73a3e44a969391be
gauliang <gauliang@outlook.com>
2018-07-23 17:26:47 -0700

    Fix a linting error.

--- scripts/lint_site.sh
+++ scripts/lint_site.sh
@@ -75,7 +75,7 @@ then
     FAILED=1
 fi
 
-#htmlproofer ./public --check-html --assume-extension --check-external-hash --check-opengraph --timeframe 2d --storage-dir .htmlproofer --url-ignore "/localhost/,/github.com/istio/istio.github.io/edit/master/,/github.com/istio/istio/issues/new/choose/,/groups.google.com/forum/"
+htmlproofer ./public --check-html --assume-extension --check-external-hash --check-opengraph --timeframe 2d --storage-dir .htmlproofer --url-ignore "/localhost/,/github.com/istio/istio.github.io/edit/master/,/github.com/istio/istio/issues/new/choose/,/groups.google.com/forum/"
 if [ "$?" != "0" ]
 then
     FAILED=1
