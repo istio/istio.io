1f417a9f41926c4a9918f9d5b7859b2c26ec704e
mtail <mtail@google.com>
2019-02-06 20:48:30 -0800

    Update to Hugo 0.54

--- scripts/build_and_push_docker.sh
+++ scripts/build_and_push_docker.sh
@@ -4,4 +4,4 @@ HUB=gcr.io/istio-testing
 VERSION=$(date +%Y-%m-%d)
 
 docker build --no-cache -t $HUB/website-builder:$VERSION .
-gcloud docker -- push $HUB/website-builder:$VERSION
+docker push $HUB/website-builder:$VERSION
