efa03cc30359ca32158179f01d36bb192567afab
Martin Taillefer <geeknoid@users.noreply.github.com>
2019-02-21 09:40:10 -0800

    Eliminate the use of bootstrap and jquery. (#3323)
    This removes two very large dependencies which ends up making
    each page load considerably faster, especially on mobile. Plus,
    not using bootstrap actually made several things simpler throughout
    the site's logic.

--- src/js/sidebar.js
+++ src/js/sidebar.js
@@ -2,15 +2,50 @@
 
 // Attach the event handlers to support the sidebar
 document.addEventListener('DOMContentLoaded', () => {
+    const sidebar = document.getElementById('sidebar');
+
     // toggle subtree in sidebar
-    document.querySelectorAll('.tree-toggle').forEach(o => {
+    sidebar.querySelectorAll('.tree-toggle').forEach(o => {
         o.addEventListener("click", () => {
             o.querySelectorAll('i.chevron').forEach(chevron => {
                 chevron.classList.toggle('show');
             });
 
             o.nextElementSibling.classList.toggle("show");
+
+            let e = o.parentElement;
+            while (!e.classList.contains('body')) {
+                e = e.parentElement;
+            }
+
+            // adjust the body's max height to the total size of the body's content
+            e.style.maxHeight = e.scrollHeight + "px";
         });
+    });
+
+    // expand/collapse cards
+    sidebar.querySelectorAll('.header').forEach(header => {
+        header.addEventListener("click", () => {
+            const body = header.nextElementSibling;
+
+            body.classList.toggle('show');
+            if (body.classList.contains('show')) {
+                // set this as the limit for expansion
+                body.style.maxHeight = body.scrollHeight + "px";
+            } else {
+                // if was expanded, reset this
+                body.style.maxHeight = null;
+            }
+        });
+    });
+
+    // force expand the default cards
+    sidebar.querySelectorAll('.body').forEach(body => {
+        if (body.classList.contains("default")) {
+            body.style.maxHeight = body.scrollHeight + "px";
+            body.classList.toggle("default");
+            body.classList.toggle("show");
+        }
     });
 
     // toggle sidebar on/off
