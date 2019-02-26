74a5f8b409e86c30683998389c17537596e0c129
gauliang <gauliang@outlook.com>
2019-02-26 06:53:57 -0800

    Add a new link check to prevent links to specific lines in GitHub files. (#3387)

--- scripts/lint_site.sh
+++ scripts/lint_site.sh
@@ -108,6 +108,14 @@ do
         echo "Ensure markdown content only uses {{< tip >}}, {{< warning >}}, {{< idea >}}, and {{< quote >}} instead of block quotes"
         FAILED=1
     fi
+
+    grep -e "\"https://github.*#L[0-9]*\"" $f
+    if [[ "$?" == "0" ]]
+    then
+        echo $f
+        echo "Ensure markdown doesn't use links to specific lines in GitHub files as those are too brittle"
+        FAILED=1
+    fi
 done
 
 htmlproofer ./public --assume-extension --check-html --disable_external ${DISABLE_EXTERNAL} --check-external-hash --check-opengraph --timeframe 2d --storage-dir .htmlproofer --url-ignore "/localhost/,/github.com/istio/istio.io/edit/master/,/github.com/istio/istio/issues/new/choose/,/groups.google.com/forum/,/www.trulia.com/"
