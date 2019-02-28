2c9d662928fe30b70e7071392e02d8c453c66823
gauliang <gauliang@outlook.com>
2019-02-28 01:38:52 -0800

    JavaScript cleanup. (#3427)
    - Now that the scripts are loaded with 'defer', no need to trigger all
    behavior via a DOMContentLoaded event handler as the scripts will all
    naturally run after the DOM has been loaded.
    
    - Use a considerably simpler approach to copy to the clipboard in code
    blocks. This saves around 10K of script code believe it or not.

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,11 +1,13 @@
 "use strict";
 
 // Attach the event handlers to support menus
-onDOMLoaded(() => {
+function handleMenu() {
     queryAll(document, '.menu').forEach(menu => {
         listen(query(menu, ".menu-trigger"), click, e => {
             e.cancelBubble = true;
             toggleOverlay(menu);
         });
     });
-});
+}
+
+handleMenu();
--- src/js/scroll.js
+++ src/js/scroll.js
@@ -5,11 +5,80 @@ let scrollToTopButton;
 let tocLinks;
 let tocHeadings;
 
-// what we do when the user scrolls the page
-listen(window, "scroll", handlePageScroll);
+function handleScroll() {
+    function handlePageScroll() {
+        // Based on the scroll position, make the "scroll to top" button visible or not
+        function controlScrollToTopButton() {
+            if (scrollToTopButton) {
+                if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+                    scrollToTopButton.classList.add('show');
+                } else {
+                    scrollToTopButton.classList.remove('show');
+                }
+            }
+        }
 
-// discover a few DOM elements up front so we don't need to do it a zillion times for the life of the page
-onDOMLoaded(() => {
+        // Based on the scroll position, activate a TOC entry
+        function controlTOCActivation() {
+            if (tocLinks) {
+                let closestHeadingBelowTop = -1;
+                let closestHeadingBelowTopPos = 1000000;
+                let closestHeadingAboveTop = -1;
+                let closestHeadingAboveTopPos = -1000000;
+
+                for (let i = 0; i < tocLinks.length; i++) {
+                    const heading = tocHeadings[i];
+                    if (heading === null) {
+                        continue;
+                    }
+
+                    const cbr = heading.getBoundingClientRect();
+
+                    if (cbr.width || cbr.height) {
+                        if ((cbr.top >= 0) && (cbr.top < window.innerHeight)) {
+                            // heading is on the screen
+                            if (cbr.top < closestHeadingBelowTopPos) {
+                                closestHeadingBelowTop = i;
+                                closestHeadingBelowTopPos = cbr.top;
+                            }
+                        } else if (cbr.top < 0) {
+                            // heading is above the screen
+                            if (cbr.top > closestHeadingAboveTopPos) {
+                                closestHeadingAboveTop = i;
+                                closestHeadingAboveTopPos = cbr.top;
+                            }
+                        }
+                    }
+
+                    tocLinks[i].classList.remove("current");
+                }
+
+                if (closestHeadingBelowTop >= 0) {
+                    tocLinks[closestHeadingBelowTop].classList.add("current");
+                } else if (closestHeadingAboveTop >= 0) {
+                    tocLinks[closestHeadingAboveTop].classList.add("current");
+                }
+            }
+        }
+
+        controlScrollToTopButton();
+        controlTOCActivation();
+
+        // HACK ALERT! When deep linking to a table row, the row ends up under the page header. This
+        // hack is here to detect that case and force-scroll the row into view.
+        //
+        // Note that this only works once for a given target row per page load. If the user is clicking
+        // around within a page, the second click to the same deep link will not trigger this hack and
+        // the user will be left with the row under the page header.
+        const target = query(document, ":target");
+        if (target && target.tagName === 'TR' && !target.dataset.scrolled) {
+            document.documentElement.scrollTop -= 55;   // where 55 is the approximate header height
+            target.dataset.scrolled = 'true';
+        }
+    }
+
+    // discover a few DOM elements up front so we don't need to do it a zillion times for the life of the page
+
     scrollToTopButton = getById('scroll-to-top');
     listen(scrollToTopButton, click, () => {
         // scroll the document to the top
@@ -29,75 +98,9 @@ onDOMLoaded(() => {
 
     // make sure things look right if we load a page to a specific anchor position
     handlePageScroll();
-});
 
-function handlePageScroll() {
-    // Based on the scroll position, make the "scroll to top" button visible or not
-    function controlScrollToTopButton() {
-        if (scrollToTopButton) {
-            if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
-                scrollToTopButton.classList.add('show');
-            } else {
-                scrollToTopButton.classList.remove('show');
-            }
-        }
-    }
-
-    // Based on the scroll position, activate a TOC entry
-    function controlTOCActivation() {
-        if (tocLinks) {
-            let closestHeadingBelowTop = -1;
-            let closestHeadingBelowTopPos = 1000000;
-            let closestHeadingAboveTop = -1;
-            let closestHeadingAboveTopPos = -1000000;
-
-            for (let i = 0; i < tocLinks.length; i++) {
-                const heading = tocHeadings[i];
-                if (heading === null) {
-                    continue;
-                }
-
-                const cbr = heading.getBoundingClientRect();
-
-                if (cbr.width || cbr.height) {
-                    if ((cbr.top >= 0) && (cbr.top < window.innerHeight)) {
-                        // heading is on the screen
-                        if (cbr.top < closestHeadingBelowTopPos) {
-                            closestHeadingBelowTop = i;
-                            closestHeadingBelowTopPos = cbr.top;
-                        }
-                    } else if (cbr.top < 0) {
-                        // heading is above the screen
-                        if (cbr.top > closestHeadingAboveTopPos) {
-                            closestHeadingAboveTop = i;
-                            closestHeadingAboveTopPos = cbr.top;
-                        }
-                    }
-                }
-
-                tocLinks[i].classList.remove("current");
-            }
-
-            if (closestHeadingBelowTop >= 0) {
-                tocLinks[closestHeadingBelowTop].classList.add("current");
-            } else if (closestHeadingAboveTop >= 0) {
-                tocLinks[closestHeadingAboveTop].classList.add("current");
-            }
-        }
-    }
-
-    controlScrollToTopButton();
-    controlTOCActivation();
-
-    // HACK ALERT! When deep linking to a table row, the row ends up under the page header. This
-    // hack is here to detect that case and force-scroll the row into view.
-    //
-    // Note that this only works once for a given target row per page load. If the user is clicking
-    // around within a page, the second click to the same deep link will not trigger this hack and
-    // the user will be left with the row under the page header.
-    const target = query(document, ":target");
-    if (target && target.tagName === 'TR' && !target.dataset.scrolled) {
-        document.documentElement.scrollTop -= 55;   // where 55 is the approximate header height
-        target.dataset.scrolled = 'true';
-    }
+    // what we do when the user scrolls the page
+    listen(window, "scroll", handlePageScroll);
 }
+
+handleScroll();
