5a9f78e843bc1f72f2e89ca7f82884a5e9c73f5c
Martin Taillefer <geeknoid@users.noreply.github.com>
2019-03-02 17:39:32 -0800

    Add a fast language switcher button in preliminary mode. (#3464)
    Also, when switching languages, the site now stays at the same page if possible.
    This should help with translation work.

---
+++ src/js/lang.js
@@ -0,0 +1,45 @@
+"use strict";
+
+function handleLanguageSwitch() {
+    listen(getById("switch-lang"), click, () => {
+        const url = new URL(window.location.href);
+        let path = url.pathname;
+        if (path.startsWith("/zh")) {
+            path = path.substr(3);
+//            createCookie("nf_lang", "en");
+        } else {
+            path = '/zh' + path;
+//            createCookie("nf_lang", "zh");
+        }
+        url.pathname = path;
+
+        navigateToUrlOrRoot(url.toString());
+        return true;
+    });
+
+    listen(getById("switch-lang-en"), click, () => {
+        const url = new URL(window.location.href);
+        let path = url.pathname;
+        if (path.startsWith("/zh")) {
+            path = path.substr(3);
+        }
+        url.pathname = path;
+
+        createCookie("nf_lang", "en");
+        navigateToUrlOrRoot(url.toString());
+    });
+
+    listen(getById("switch-lang-zh"), click, () => {
+        const url = new URL(window.location.href);
+        let path = url.pathname;
+        if (!path.startsWith("/zh")) {
+            path = '/zh' + path;
+        }
+        url.pathname = path;
+
+        createCookie("nf_lang", "zh");
+        navigateToUrlOrRoot(url.toString());
+    });
+}
+
+handleLanguageSwitch();
