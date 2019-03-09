2319402009da6c11c0a732ad970213a6b3c76444
gauliang <gauliang@outlook.com>
2019-03-09 07:31:29 -0800

    Bunch of site improvements. (#3601)
    - Work on keyboard navigation support. It's not totally done yet,
    but its a lot better than it's ever been.
    
    - Fix incorrect font use for code blocks. They were coming out way
    smaller than intended.
    
    - Improve the HTML in a variety of ways to make it better for
    screen readers.

--- src/js/constants.js
+++ src/js/constants.js
@@ -3,3 +3,6 @@ const mouseenter = 'mouseenter';
 const mouseleave = 'mouseleave';
 const active = 'active';
 const keyup = 'keyup';
+const button = 'button';
+const ariaLabel = 'aria-label';
+const ariaExpanded = 'aria-expanded';
