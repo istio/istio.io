0db86470551bb1ea33e58d32e64f779c3976c03e
mtail <mtail@google.com>
2019-03-10 10:20:03 -0700

    Fix tab problem.

--- src/js/tabs.js
+++ src/js/tabs.js
@@ -1,18 +1,14 @@
 "use strict";
 
 function handleTabs() {
-    queryAll(document, "[role=tablist]").forEach(tabset => {
-        queryAll(tabset, ".tab-strip").forEach(strip => {
-            const cookieName = strip.dataset.cookieName;
 
-            // select the active tabs on the page per cookie values
-            if (cookieName) {
-                const cookieValue = readCookie(cookieName);
-                if (cookieValue) {
+    function updateLikeTabsets(cookieName, cookieValue) {
+        queryAll(document, "[role=tablist]").forEach(tabset => {
+            queryAll(tabset, ".tab-strip").forEach(strip => {
+                if (strip.dataset.cookieName === cookieName) {
                     queryAll(strip, button).forEach(tab => {
                         if (tab.dataset.cookieValue === cookieValue) {
                             tab.classList.add(active);
-                            tab.tabIndex = -1;
                             getById(tab.dataset.tab).classList.add(active);
                         } else {
                             tab.classList.remove(active);
@@ -20,22 +16,33 @@ function handleTabs() {
                         }
                     });
                 }
+            });
+        });
+    }
+
+    queryAll(document, "[role=tablist]").forEach(tabset => {
+        queryAll(tabset, ".tab-strip").forEach(strip => {
+            const cookieName = strip.dataset.cookieName;
+            if (cookieName) {
+                const cookieValue = readCookie(cookieName);
+                if (cookieValue) {
+                    updateLikeTabsets(cookieName, cookieValue);
+                }
             }
 
             // attach the event handlers to support tab sets
             queryAll(strip, button).forEach(tab => {
                 listen(tab, click, () => {
                     queryAll(strip, button).forEach(tab2 => {
-                        tsb2.classList.remove(active);
-                        tab2.tabIndex = 0;
+                        tab2.classList.remove(active);
                         getById(tab2.dataset.tab).classList.remove(active);
                     });
 
                     tab.classList.add(active);
-                    tab.tabIndex = -1;
                     getById(tab.dataset.tab).classList.add(active);
                     if (cookieName !== null) {
                         createCookie(cookieName, tab.dataset.cookieValue);
+                        updateLikeTabsets(cookieName, tab.dataset.cookieValue);
                     }
                 });
             });
