f19286e5d550fa5bc44e7f317958ee9c4afe835b
mtail <mtail@google.com>
2019-03-20 08:00:35 -0700

    Use server-side redirects instead of alias files for better experience.

--- scripts/gen_site.sh
+++ scripts/gen_site.sh
@@ -3,12 +3,6 @@ set -e
 
 hugo version
 
-if [[ "$3" == "-no_aliases" ]]
-then
-    HUGO_DISABLEALIASES=true
-    export HUGO_DISABLEALIASES
-fi
-
 if [[ "$2" == "-no_minify" ]]
 then
     hugo --baseURL "$1"
