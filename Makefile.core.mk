182f5d90ee339ec34288b7e2d61c933b1e17e4a3
Steven Dake <sdake@ibm.com>
2019-08-15 16:33:21 -0700

    Make netlify work properly

--- Makefile.core.mk
+++ Makefile.core.mk
@@ -35,8 +35,17 @@ serve: build
 	@docker run -t -i --sig-proxy=true --rm --user $(uid) -v /etc/passwd:/etc/passwd:ro -v $(shell pwd):/site -w /site -p 1313:1313 $(img) hugo serve --baseURL "http://${ISTIO_SERVE_DOMAIN}:1313/" --bind 0.0.0.0 --disableFastRender
 
 install:
-	@npm install -g sass sass-lint typescript tslint @babel/cli @babel/core svgstore-cli
-	@npm install babel-preset-minify --save-dev
+	@npm init -y
+	@npm install -g \
+	    sass \
+	    sass-lint \
+	    typescript \
+	    tslint \
+	    markdown-spellcheck \
+	    svgstore-cli \
+	    svgo
+	@npm install --save-dev @babel/core @babel/cli @babel/preset-env
+	@npm install --save @babel/polyfill
 
 netlify: install
 	@scripts/build_site.sh
