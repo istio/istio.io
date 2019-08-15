09041ac0ac87ee275ed46c457394108123212ac0
Steven Dake <sdake@ibm.com>
2019-08-15 16:30:47 -0700

    Take 2

--- scripts/build_site.sh
+++ scripts/build_site.sh
@@ -9,5 +9,5 @@ npx tsc
 
 npx babel-cli --source-maps --minified --no-comments tmp/js/constants.js tmp/js/utils.js tmp/js/kbdnav.js tmp/js/themes.js tmp/js/menu.js tmp/js/header.js tmp/js/sidebar.js tmp/js/tabset.js tmp/js/prism.js tmp/js/codeBlocks.js tmp/js/links.js tmp/js/scroll.js tmp/js/overlays.js tmp/js/lang.js tmp/js/callToAction.js --out-file generated/js/all.min.js
 
-npx babel-cli --source-maps --minified --no-comments --out-file tmp/js/themes_init.js generated/js/themes_init.min.js
+npx babel-cli --source-maps --minified --no-comments tmp/js/themes_init.js --out-file generated/js/themes_init.min.js
 npx svgstore -o generated/img/icons.svg src/icons/**/*.svg
