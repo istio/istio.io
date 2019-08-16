1c4e8bf523c1998592b6ebe9e5f61cd0377b16f1
Martin Taillefer <geeknoid@users.noreply.github.com>
2019-08-16 07:14:29 -0700

    Get istio.io working with BUILD_WITH_CONTAINER=1 (#4804)
    The default for this repo is to build with the container.

--- Makefile.core.mk
+++ Makefile.core.mk
@@ -1,26 +1,21 @@
 ISTIO_SERVE_DOMAIN ?= localhost
 export ISTIO_SERVE_DOMAIN
 
-img := gcr.io/istio-testing/build-tools:2019-08-15
-uid := $(shell id -u)
-docker := docker run -e INTERNAL_ONLY=true -t -i --sig-proxy=true --rm --user $(uid) -v /etc/passwd:/etc/passwd:ro -v $(shell pwd):/site -w /site $(img)
-
-ifeq ($(INTERNAL_ONLY),)
-docker := docker run -t -i --sig-proxy=true --rm --user $(uid) -v /etc/passwd:/etc/passwd:ro -v $(shell pwd):/site -w /site $(img)
-endif
-
 ifeq ($(CONTEXT),production)
 baseurl := "$(URL)"
 endif
 
 build:
-	@$(docker) scripts/build_site.sh
+	@scripts/build_site.sh
 
 gen: build
-	@$(docker) scripts/gen_site.sh ""
+	@scripts/gen_site.sh ""
+
+gen_nominify: build
+	@scripts/gen_site.sh "" -no_minify
 
 opt:
-	@$(docker) scripts/opt_site.sh
+	@scripts/opt_site.sh
 
 clean_public:
 	@rm -fr public
@@ -28,11 +23,11 @@ clean_public:
 clean: clean_public
 	@rm -fr resources .htmlproofer tmp
 
-lint: clean_public build gen
-	@$(docker) scripts/lint_site.sh
+lint: clean_public gen_nominify
+	@scripts/lint_site.sh
 
 serve: build
-	@docker run -t -i --sig-proxy=true --rm --user $(uid) -v /etc/passwd:/etc/passwd:ro -v $(shell pwd):/site -w /site -p 1313:1313 $(img) hugo serve --baseURL "http://${ISTIO_SERVE_DOMAIN}:1313/" --bind 0.0.0.0 --disableFastRender
+	@hugo serve --baseURL "http://${ISTIO_SERVE_DOMAIN}:1313/" --bind 0.0.0.0 --disableFastRender
 
 install:
 	@npm init -y
@@ -55,11 +50,8 @@ netlify_archive:
 	@scripts/gen_archive_site.sh "$(baseurl)"
 
 archive:
-	@$(docker) scripts/gen_archive_site.sh "$(baseurl)"
+	@scripts/gen_archive_site.sh "$(baseurl)"
 
-prow:
-	@scripts/build_site.sh
-	@scripts/gen_site.sh "" -no_minify
-	@scripts/lint_site.sh
+prow: lint
 
 include Makefile.common.mk
