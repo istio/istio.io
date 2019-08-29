c007e7a740880d34e81b71cdc5f3812255f57c02
gauliang <gauliang@outlook.com>
2019-08-29 08:09:32 -0700

    Update common files (#4870)
    - Enable a bunch of linting rules in the repo, and addressed all the linting
    errors

--- scripts/lint_site.sh
+++ scripts/lint_site.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+# Copyright Istio Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -e
+
 FAILED=0
 
 echo -ne "mdspell "
@@ -22,123 +38,99 @@ check_content() {
     TMP=$(mktemp -d)
 
     # make the tmp dir
-    mkdir -p ${TMP}
+    mkdir -p "${TMP}"
 
     # create a throwaway copy of the content
-    cp -R ${DIR} ${TMP}
-    cp .spelling ${TMP}
-    cp mdl_style.rb ${TMP}
+    cp -R "${DIR}" "${TMP}"
+    cp .spelling "${TMP}"
+    cp mdl_style.rb "${TMP}"
 
     # replace the {{< text >}} shortcodes with ```plain
-    find ${TMP} -type f -name \*.md -exec sed -E -i "s/\\{\\{< text .*>\}\}/\`\`\`plain/g" {} ";"
+    find "${TMP}" -type f -name \*.md -exec sed -E -i "s/\\{\\{< text .*>\}\}/\`\`\`plain/g" {} ";"
 
     # replace the {{< /text >}} shortcodes with ```
-    find ${TMP} -type f -name \*.md -exec sed -E -i "s/\\{\\{< \/text .*>\}\}/\`\`\`/g" {} ";"
+    find "${TMP}" -type f -name \*.md -exec sed -E -i "s/\\{\\{< \/text .*>\}\}/\`\`\`/g" {} ";"
 
     # elide url="*"
-    find ${TMP} -type f -name \*.md -exec sed -E -i "s/url=\".*\"/URL/g" {} ";"
+    find "${TMP}" -type f -name \*.md -exec sed -E -i "s/url=\".*\"/URL/g" {} ";"
 
     # elide link="*"
-    find ${TMP} -type f -name \*.md -exec sed -E -i "s/link=\".*\"/LINK/g" {} ";"
+    find "${TMP}" -type f -name \*.md -exec sed -E -i "s/link=\".*\"/LINK/g" {} ";"
 
     # switch to the temp dir
-    pushd ${TMP} >/dev/null
+    pushd "${TMP}" >/dev/null
 
-    mdspell ${LANG} --ignore-acronyms --ignore-numbers --no-suggestions --report `find content/en -type f -name '*.md'`
-    if [[ "$?" != "0" ]]
-    then
+    if ! mdspell "${LANG}" --ignore-acronyms --ignore-numbers --no-suggestions --report $(find content/en -type f -name '*.md'); then
         echo "To learn how to address spelling errors, please see https://istio.io/about/contribute/creating-and-editing-pages/#linting"
         FAILED=1
     fi
 
-    mdl --ignore-front-matter --style mdl_style.rb .
-    if [[ "$?" != "0" ]]
-    then
+    if ! mdl --ignore-front-matter --style mdl_style.rb .; then
         FAILED=1
     fi
 
-    grep -nr -e "(https://istio.io" .
-    if [[ "$?" == "0" ]]
-    then
+    if grep -nr -e "(https://istio.io" .; then
         echo "Ensure markdown content uses relative references to istio.io"
         FAILED=1
     fi
 
-    grep -nr -e "(https://preliminary.istio.io" .
-    if [[ "$?" == "0" ]]
-    then
+    if grep -nr -e "(https://preliminary.istio.io" .; then
         echo "Ensure markdown content doesn't contain references to preliminary.istio.io"
         FAILED=1
     fi
 
-    grep -nr -e "https://github.com/istio/istio/blob/" .
-    if [[ "$?" == "0" ]]
-    then
+    if grep -nr -e https://github.com/istio/istio/blob/ .; then
         echo "Ensure markdown content uses {{< github_blob >}}"
         FAILED=1
     fi
 
-    grep -nr -e "https://github.com/istio/istio/tree/" .
-    if [[ "$?" == "0" ]]
-    then
+    if grep -nr -e https://github.com/istio/istio/tree/ .; then
         echo "Ensure markdown content uses {{< github_tree >}}"
         FAILED=1
     fi
 
-    grep -nr -e "https://raw.githubusercontent.com/istio/istio/" .
-    if [[ "$?" == "0" ]]
-    then
+    if grep -nr -e https://raw.githubusercontent.com/istio/istio/ .; then
         echo "Ensure markdown content uses {{< github_file >}}"
         FAILED=1
     fi
 
     # go back whence we came
-    popd  >/dev/null
+    popd >/dev/null
 
     # cleanup
-    rm -fr ${TMP}
+    rm -fr "${TMP}"
 }
 
 check_content content --en-us
 
-for f in `find ./content/en -type f \( -name '*.html' -o -name '*.md' \)`
-do
-    grep -H -n -e "“" ${f}
-    if [[ "$?" == "0" ]]
-    then
+for f in $(find ./content/en -type f \( -name '*.html' -o -name '*.md' \)); do
+    # shellcheck disable=SC1111
+    if grep -H -n -e '“' "${f}"; then
         echo "Ensure content only uses standard quotation marks and not “"
         FAILED=1
     fi
 done
 
-for f in `find ./public -type f -name '*.html'`
-do
-    grep -H -n -i -e "blockquote" ${f}
-    if [[ "$?" == "0" ]]
-    then
+for f in $(find ./public -type f -name '*.html'); do
+      if grep -H -n -i -e blockquote "${f}"; then
         echo "Ensure content only uses {{< tip >}}, {{< warning >}}, {{< idea >}}, and {{< quote >}} instead of block quotes"
         FAILED=1
     fi
 
-    grep -H -n -e "\"https://github.*#L[0-9]*\"" ${f}
-    if [[ "$?" == "0" ]]
-    then
+    if grep -H -n -e "\"https://github.*#L[0-9]*\"" "${f}"; then
         echo "Ensure content doesn't use links to specific lines in GitHub files as those are too brittle"
         FAILED=1
     fi
 done
 
-sass-lint -c sass-lint.yml --verbose 'src/sass/**/*.scss'
+sass-lint -c sass-lint.yml --verbose src/sass/**/*.scss
 tslint src/ts/*.ts
 
-htmlproofer ./public --assume-extension --check-html --check-external-hash --check-opengraph --timeframe 2d --storage-dir .htmlproofer --url-ignore "/localhost/,/github.com/istio/istio.io/edit/,/github.com/istio/istio/issues/new/choose/,/groups.google.com/forum/,/www.trulia.com/,/apporbit.com/"
-if [[ "$?" != "0" ]]
-then
+if ! htmlproofer ./public --assume-extension --check-html --check-external-hash --check-opengraph --timeframe 2d --storage-dir .htmlproofer --url-ignore "/localhost/,/github.com/istio/istio.io/edit/,/github.com/istio/istio/issues/new/choose/,/groups.google.com/forum/,/www.trulia.com/,/apporbit.com/"; then
     FAILED=1
 fi
 
-if [[ ${FAILED} -eq 1 ]]
-then
+if [[ ${FAILED} -eq 1 ]]; then
     echo "LINTING FAILED"
     exit 1
 fi
