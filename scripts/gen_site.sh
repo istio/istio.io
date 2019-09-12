31f765b7829ea170f848b31ba13acf655c1ff6ec
Martin Taillefer <mtail@google.com>
2019-09-12 11:50:55 -0700

    Remove minifier since it's no longer present and not worth fixing.

--- scripts/gen_site.sh
+++ scripts/gen_site.sh
@@ -2,4 +2,3 @@
 set -e
 
 hugo --baseURL "$1"
-html-minifier --input-dir public --output-dir public --file-ext html --collapse-whitespace --minify-js --minify-css --sort-attributes --sort-class-name --remove-attribute-quotes --remove-comments
