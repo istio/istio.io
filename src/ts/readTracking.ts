2c1f506e391683768a6b979292f3b287971c179e
Martin Taillefer <geeknoid@users.noreply.github.com>
2019-11-19 11:44:46 -0800

    Add support for dots & pills for both news and blog posts. (#5768)
    - If a returning user comes to the site, if there are unread
    blog posts or news articles less than 15 quadrllion nanosecond
    old will be treated as being unread. When there are unred articles,
    the News or Blog link in the title bar will get a green dot indicating
    articles are available. When clicking on News, then you'll get the
    news categories with a pill showing how many articles are unread for
    each category.
    
    First-time visitors to the site will not get any dots or pills for
    existing articles. These will only appear in subsequent visits for
    new articles.
    
    Due to the default behavior for new users, if you just look at the
    preview, you will not see any pills or dots. To see what this actually
    looks like, load up the preview, then go to the Chrome Developer Tools,
    click on the Application tab, then on Local Storage, and then find the
    visitedPages entry. Right click on the entry, select Edit Value,
    and set the value to {}. Then refresh the page and you
    should see some dots show up next to the Blog and News links in
    the header.
    
    (cherry picked from commit 6cdafbeb3d1679f2850f83beb865817cb554d0a3)

---
+++ src/ts/readTracking.ts
@@ -0,0 +1,183 @@
+// Copyright Istio Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+let trackedPages: any = null;
+let visitedPages: any = null;
+
+function loadVisitedPages(): void {
+    const blob = localStorage.getItem("visitedPages");
+    if (blob != null) {
+        visitedPages = JSON.parse(blob);
+    }
+}
+
+function saveVisitedPages(): void {
+    localStorage.setItem("visitedPages", JSON.stringify(visitedPages));
+}
+
+function markPagesAsVisited(prefix: string): void {
+    if (trackedPages !== null) {
+        let dirty = false;
+        for (const trackedPage in trackedPages) {
+            if (trackedPages.hasOwnProperty(trackedPage)) {
+                if (trackedPage.startsWith(prefix)) {
+                    visitedPages[trackedPage] = 1;
+                    dirty = true;
+                }
+            }
+        }
+
+        if (dirty) {
+            saveVisitedPages();
+            setPills();
+            setDots();
+            setMarkAllRead();
+        }
+    }
+}
+
+function setPills(): void {
+    document.querySelectorAll<HTMLElement>(".pill").forEach(pill => {
+        const prefix = pill.dataset.prefix;
+        if (prefix === undefined) {
+            return;
+        }
+
+        const count = countUnvisited(prefix);
+        if (count > 0) {
+            pill.classList.add("visible");
+            pill.innerText = count.toString();
+        } else {
+            pill.classList.remove("visible");
+        }
+    });
+}
+
+function setDots(): void {
+    document.querySelectorAll<HTMLElement>(".dot").forEach(dot => {
+        const prefix = dot.dataset.prefix;
+        if (prefix === undefined) {
+            return;
+        }
+
+        const count = countUnvisited(prefix);
+        if (count > 0) {
+            dot.classList.add("visible");
+        } else {
+            dot.classList.remove("visible");
+        }
+    });
+}
+
+function setMarkAllRead(): void {
+    const button = document.getElementById("mark-all-read");
+    if (button != null) {
+        const prefix = button.dataset.prefix;
+        if (prefix === undefined) {
+            return;
+        }
+
+        const count = countUnvisited(prefix);
+        if (count > 0) {
+            button.classList.add("visible");
+        } else {
+            button.classList.remove("visible");
+        }
+    }
+}
+
+function countUnvisited(prefix: string): number {
+    let count = 0;
+    for (const trackedPage in trackedPages) {
+        if (trackedPages.hasOwnProperty(trackedPage)) {
+            if (trackedPage.startsWith(prefix)) {
+                let found = false;
+                for (const visitedPage in visitedPages) {
+                    if (trackedPage === visitedPage) {
+                        found = true;
+                        break;
+                    }
+                }
+
+                if (!found) {
+                    count++;
+                }
+            }
+        }
+    }
+
+    return count;
+}
+
+function handleReadTracking(): void {
+    // Asynchronously loads the set of tracked pages, which are the pages we
+    // consider for pills
+    function fetchTrackedPagesIndex(): void {
+        fetch("/_tracked_pages.json")
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error("HTTP error " + response.status);
+                }
+                return response.json();
+            })
+            .then(json => {
+                trackedPages = json;
+
+                let dirty = false;
+                if (visitedPages === null) {
+                    // if we didn't find any list of visited pages, initialize it to the set of tracked pages
+                    visitedPages = new Map();
+
+                    for (const trackedPage in trackedPages) {
+                        if (trackedPages.hasOwnProperty(trackedPage)) {
+                            visitedPages[trackedPage] = 1;
+                        }
+                    }
+                    dirty = true;
+                } else {
+                    // if we did find a list of visited pages, trim it to only contain what's in the tracked pages map
+                    for (const visitedPage in visitedPages) {
+                        if (visitedPages.hasOwnProperty(visitedPage)) {
+                            if (trackedPages[visitedPage] === undefined) {
+                                visitedPages.delete(visitedPage);
+                                dirty = true;
+                            }
+                        }
+                    }
+                }
+
+                // if the current page is being tracked, record that the user has visited this page
+                const page = window.location.pathname;
+                if (trackedPages[page] !== undefined) {
+                    if (visitedPages[page] === undefined) {
+                        visitedPages[page] = 1;
+                        dirty = true;
+                    }
+                }
+
+                if (dirty) {
+                    saveVisitedPages();
+                }
+
+                setPills();
+                setDots();
+                setMarkAllRead();
+            });
+    }
+
+    loadVisitedPages();
+    fetchTrackedPagesIndex();
+}
+
+handleReadTracking();
