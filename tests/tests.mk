04944b5c21773a0d35ca043fdc1ce5041143b258
nmittler <nmittler@gmail.com>
2020-02-26 11:19:15 -0800

    More fixes and cleanup for makefiles

--- tests/tests.mk
+++ tests/tests.mk
@@ -31,11 +31,8 @@ endif
 # $(INTEGRATION_TEST_KUBECONFIG) specifies the kube config file to be used. If not specified, then
 # ~/.kube/config is used.
 # TODO: This probably needs to be more intelligent and take environment variables into account.
-ifneq ($(KUBECONFIG),)
-	_INTEGRATION_TEST_FLAGS += --istio.test.kube.config=$(KUBECONFIG)
-else
-	_INTEGRATION_TEST_FLAGS += --istio.test.kube.config=~/.kube/config
-endif
+KUBECONFIG ?= ~/.kube/config
+_INTEGRATION_TEST_FLAGS += --istio.test.kube.config=$(KUBECONFIG)
 
 test.kube.presubmit: init
 	PATH=${PATH}:${ISTIO_OUT} $(GO) test -p 1 ${T} ./tests/... -timeout 30m \
