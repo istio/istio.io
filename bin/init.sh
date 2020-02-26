a804d49342fabe0193dd7bce476f39dc36d798b2
Nathan Mittler <nmittler@gmail.com>
2020-02-26 14:55:25 -0800

    More fixes and cleanup for makefiles (#6612)

--- bin/init.sh
+++ bin/init.sh
@@ -18,29 +18,62 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+export ISTIO_REMOTE=${ISTIO_REMOTE:-origin}
 export ISTIO_BRANCH=${ISTIO_BRANCH:-master}
 
 echo "ISTIOIO_GO=${ISTIOIO_GO}"
 echo "ISTIO_GO=${ISTIO_GO}"
+echo "ISTIO_REMOTE=${ISTIO_REMOTE}"
 echo "ISTIO_BRANCH=${ISTIO_BRANCH}"
 
 # Download the Istio source if not available.
-if [[ ! -d "${ISTIO_GO}" ]]
+if [[ -d "${ISTIO_GO}" ]]
 then
-    git clone https://github.com/istio/istio.git "${ISTIO_GO}"
+  echo "${ISTIO_GO} already exists. Using existing repository ..."
+else
+  echo "${ISTIO_GO} not found. Cloning Istio repository ..."
+  git clone https://github.com/istio/istio.git "${ISTIO_GO}"
+fi
+
+pushd "${ISTIO_GO}" > /dev/null
+
+REMOTE_BRANCH="${ISTIO_REMOTE}/${ISTIO_BRANCH}"
+
+# Switch to latest for requested branch if possible
+if [ -z "$(git status --porcelain)" ]; then
+  # Get updates to the remote repository
+  git fetch "$ISTIO_REMOTE"
+
+  # Switch to the desired local branch (create if necessary)
+  git checkout -B "${ISTIO_BRANCH}"
+
+  LOCAL_REV=$(git rev-parse @)
+  REMOTE_REV=$(git rev-parse "${REMOTE_BRANCH}")
+  BASE_REV=$(git merge-base @ "${REMOTE_BRANCH}")
+
+  if [[ "${LOCAL_REV}" == "${REMOTE_REV}" ]]; then
+    echo "Istio already up-to-date with ${REMOTE_BRANCH}"
+  elif [[ "${LOCAL_REV}" == "${BASE_REV}" ]]; then
+    echo "WARNING: Istio is out-of-date with ${REMOTE_BRANCH}. Merging remote changes."
+    git pull "${ISTIO_REMOTE}" "${ISTIO_BRANCH}"
+  elif [[ "${REMOTE_REV}" == "${BASE_REV}" ]]; then
+    echo "WARNING: Istio is ahead of ${REMOTE_BRANCH}. Using local branch."
+  else
+    echo "WARNING: Istio has diverged with ${REMOTE_BRANCH}. Using local branch."
+  fi
+
+else
+  echo "WARNING: Istio has uncommitted changes. To use latest from ${REMOTE_BRANCH} clear your local changes."
 fi
 
 # Build and install istioctl
-pushd "${ISTIO_GO}"
-git checkout "${ISTIO_BRANCH}"
 go install ./istioctl/cmd/istioctl
-popd
+
+popd > /dev/null
 
 # Copy install/samples files over from Istio. These are needed by the tests.
-rm -rf "${ISTIOIO_GO}/install"
+rm -rf "${ISTIOIO_GO}/install" "${ISTIOIO_GO}/samples"
 cp -a "${ISTIO_GO}/install" "${ISTIOIO_GO}/install"
-
-rm -rf "${ISTIOIO_GO}/samples"
 cp -a "${ISTIO_GO}/samples" "${ISTIOIO_GO}/samples"
 
 # For generating junit.xml files
