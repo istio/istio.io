d59b390a499740b3e09e7dd07b59f2ffbef3dcdd
Eric Van Norman <ericvn@us.ibm.com>
2020-04-29 10:18:13 -0500

    Update lint_site to only check languages for files in current PR (#7133)
    * Update lint to only check languages for files in current PR
    
    * Default branch to master if undefined, lint fixes
    
    * Fix @ in Makefile.core.mk
    
    * Add pt-br support to language check
    
    * Review comments
    
    * Apply suggestions from code review
    
    Either BRANCH is specified or a language, else it fails.
    
    Co-Authored-By: Frank Budinsky <frankb@ca.ibm.com>
    
    * Update scripts/lint_site.sh
    
    Co-Authored-By: Frank Budinsky <frankb@ca.ibm.com>
    
    * Update Makefile.core.mk
    
    Co-Authored-By: Frank Budinsky <frankb@ca.ibm.com>
    
    * Don't check languages for lint-fast if no languages are found
    
    Co-authored-by: Frank Budinsky <frankb@ca.ibm.com>

--- Makefile.core.mk
+++ Makefile.core.mk
@@ -96,13 +96,13 @@ clean:
 	@rm -fr resources .htmlproofer tmp generated public out samples install go tests/integration/
 
 lint: clean_public build_nominify lint-copyright-banner lint-python lint-yaml lint-dockerfiles lint-scripts lint-sass lint-typescript lint-go
-	@scripts/lint_site.sh
+	@BRANCH=$(SOURCE_BRANCH_NAME) scripts/lint_site.sh
 
 lint-en: clean_public build_nominify lint-copyright-banner lint-python lint-yaml lint-dockerfiles lint-scripts lint-sass lint-typescript lint-go
 	@scripts/lint_site.sh en
 
 lint-fast:
-	@SKIP_LINK_CHECK=true scripts/lint_site.sh en
+	@BRANCH=$(SOURCE_BRANCH_NAME) SKIP_LINK_CHECK=true scripts/lint_site.sh
 
 serve: site
 	@hugo serve --baseURL "http://${ISTIO_SERVE_DOMAIN}:1313/" --bind 0.0.0.0 --disableFastRender
