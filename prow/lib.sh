e04912dacf99746b7d8e2fbcad920ee21b5bbe57
John Howard <howardjohn@google.com>
2020-05-05 18:48:11 -0700

    Update kind image to 1.18.2 (#7228)
    * Update kind image to 1.18.2
    
    * Update prow/lib.sh
    
    Co-authored-by: Eric Van Norman <ericvn@us.ibm.com>

--- prow/lib.sh
+++ prow/lib.sh
@@ -60,10 +60,10 @@ function cleanup_kind_cluster() {
 }
 
 function setup_kind_cluster() {
-  IMAGE="${1:-kindest/node:v1.17.0}"
+  IMAGE="${1:-kindest/node:v1.18.2}"
   NAME="${2:-istio-testing}"
   CONFIG="${3:-}"
-  # Delete any previous e2e KinD cluster
+  # Delete previous e2e KinD cluster
   echo "Deleting previous KinD cluster with name=${NAME}"
   if ! (kind delete cluster --name="${NAME}" -v9) > /dev/null; then
     echo "No existing kind cluster with name ${NAME}. Continue..."
