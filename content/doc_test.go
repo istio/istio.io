47ab8da01a0e423e96175c9c5227dde8e437b2e6
niwatorrri <niwatori@foxmail.com>
2020-05-27 19:03:57 -0400

    main test function: save progress

---
+++ content/doc_test.go
@@ -0,0 +1,110 @@
+// Copyright 2020 Istio Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package content
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+
+	// "istio.io/istio.io/pkg/test/istioio"
+
+	"istio.io/istio.io/pkg/test/istioio"
+	"istio.io/istio/pkg/test/framework"
+	"istio.io/istio/pkg/test/framework/components/istio"
+)
+
+var inst istio.Instance
+
+var testsToRun = flag.String("test", "all", "tests to be run")
+
+func TestMain(m *testing.M) {
+	fmt.Println("Starting test docs: " + *testsToRun)
+	framework.
+		NewSuite("doctest", m).Run()
+	// SetupOnEnv(environment.Kube, istio.Setup(&inst, nil)).
+	// RequireEnvironment(environment.Kube).
+	// Run()
+}
+
+func TestDocs(t *testing.T) {
+	// 2. traverse through content/* to match the folder to be tested (or test all folders)
+	testFileSuffix := "/test.sh"
+
+	err := filepath.Walk(".",
+		func(path string, info os.FileInfo, err error) error {
+			checkFile := strings.HasSuffix(path, testFileSuffix) &&
+				(*testsToRun == "all" || strings.Contains(path, *testsToRun))
+			if checkFile {
+				runTestFile(path)
+			}
+			return nil
+		},
+	)
+	if err != nil {
+		fmt.Printf("Failed to execute tests: %s", err)
+	}
+
+	// 3. for each matched folder, find `test.sh`, parse it into test and cleanup, then run test
+
+	// 4. aggregate results and report
+	fmt.Println("Test finished")
+}
+
+func runTestFile(path string) (bool, error) {
+	fmt.Println("Running: " + path)
+
+	script, err := ioutil.ReadFile(path)
+	if err != nil {
+		return false, err
+	}
+
+	testCleanupSep := "# cleanup"
+	splitScript := strings.Split(string(script), testCleanupSep)
+	if len(splitScript) != 2 {
+		fmt.Println("Expected two-part script")
+		return false, nil
+	}
+
+	testScript := splitScript[0]
+	cleanupScript := splitScript[1]
+	// fmt.Println(testScript[len(testScript)-100:])
+	// fmt.Println(cleanupScript)
+
+	t := new(testing.T)
+	framework.
+		NewTest(t).
+		Run(istioio.NewBuilder(path).
+			Add(istioio.Script{
+				Input: istioio.Inline{
+					FileName: "test.sh",
+					Value:    testScript,
+				},
+			}).
+			Defer(istioio.Script{
+				Input: istioio.Inline{
+					FileName: "cleanup.sh",
+					Value:    cleanupScript,
+				},
+			}).
+			Build())
+
+	// report if test succeeds or files with logs
+
+	return true, nil
+}
---
+++ content/en/docs/README.md
@@ -0,0 +1,7 @@
+# How to Test Istio.io Docs
+
+The tests should be run when:
+- changes are made to the doc
+- changes are made to the istio source code
+
+// Can I remove the ./test folder?
