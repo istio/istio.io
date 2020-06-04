7eb6de0654e68599d5c17ab980062e32271c4a13
niwatorrri <niwatori@foxmail.com>
2020-06-04 17:19:58 -0400

    change package name

--- tests/doc_test.go
+++ tests/doc_test.go
@@ -11,7 +11,10 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-package content
+//
+// This is the framework for doc testing. It scans through the content
+// folder and runs each matched `tesh.sh` file as a subtest.
+package framework
 
 import (
 	"fmt"
