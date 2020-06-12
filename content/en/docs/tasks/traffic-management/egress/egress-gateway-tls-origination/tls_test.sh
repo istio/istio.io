c917e2277326989e743bfcee9079c1fb59d1bfc9
Navraj Singh Chhina <navuchhina@live.com>
2020-06-12 11:13:41 -0400

    Update content/en/docs/tasks/traffic-management/egress/egress-gateway-tls-origination/tls_test.sh
    Co-authored-by: Frank Budinsky <frankb@ca.ibm.com>

--- content/en/docs/tasks/traffic-management/egress/egress-gateway-tls-origination/tls_test.sh
+++ content/en/docs/tasks/traffic-management/egress/egress-gateway-tls-origination/tls_test.sh
@@ -21,7 +21,6 @@ set -e
 set -u
 set -o pipefail
 
-source "${REPO_ROOT}/content/en/docs/tasks/traffic-management/egress/egress-gateway-tls-origination/snips.sh"
 source "${REPO_ROOT}/tests/util/helpers.sh"
 
 # Deploy sleep sample and set up variable pointing to it
