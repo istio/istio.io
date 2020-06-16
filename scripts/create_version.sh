c14afe643bd58645ad46efc0f3c883a3e9f5be03
niwatorrri <niwatori@foxmail.com>
2020-06-16 19:08:04 -0400

    first draft

--- scripts/create_version.sh
+++ scripts/create_version.sh
@@ -54,7 +54,7 @@ echo "Upcoming version: ${NEXT}"
 
 git checkout "release-${PREV}"
 sed -i "
-    s/^archive: false/archive: true/;
+    s/^archive: false$/archive: true/;
     s/^archive_date: .*$/archive_date: $(date +'%Y-%m-%d')/;
     s/^archive_search_refinement: .*$/archive_search_refinement: \"V${PREV}\"/
 " data/args.yml
@@ -64,7 +64,51 @@ make archive-version
 
 git add data/args.yml config.toml
 git commit -m "archive the release version ${PREV}"
+
+ORIGIN="https://github.com/istio/istio.io"
+git push "${ORIGIN}"
+
+git checkout master
+sed -i "
+    s/^preliminary: .*$/preliminary: \"${NEXT}\"/;
+    s/^main: .*$/main: \"${CURR}\"/
+" data/versions.yml
+
+mv "archived_version/v${PREV}" "archive/v${PREV}"
+sed -i "0,/<li>/s//\<li>\n\
+            <a href=\/v${PREV}>v${PREV}<\/a>\n\
+        <\/li>\n\
+        <li>/" archive/archive/index.html
+
+git add data/versions.yml archive
+git commit -m "build an archive in the master branch"
 git push
 
-# git checkout master
+git checkout -b "release-${CURR}"
+sed -i "
+    s/^preliminary: true$/preliminary: false/;
+    s/^doc_branch_name: .*$/doc_branch_name: release-${CURR}/;
+    s/^source_branch_name: .*$/source_branch_name: release-${CURR}/
+" data/args.yml
 
+sed -i "s/^SOURCE_BRANCH_NAME ?=.*$/SOURCE_BRANCH_NAME ?= release-${CURR}/" Makefile.core.mk
+make update_all
+
+sed -i "s/^UPDATE_BRANCH ?=.*$/UPDATE_BRANCH ?= release-${CURR}/" common/Makefile.common.mk
+make update-common
+
+git add *
+git commit -m "create a new release branch for v${CURR}"
+git push
+
+git checkout master
+sed -i "
+    s/^version: .*$/version: \"${NEXT}\"/;
+    s/^full_version: .*$/full_version: \"${NEXT}.0\"/;
+    s/^previous_version: .*$/previous_version: \"${CURR}\"/
+" data/args.yml
+make update_all
+
+git add *
+git commit -m "advance master to the next release v${NEXT}"
+git push
