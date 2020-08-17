dfae9006429c6eb606ebd5d6dcee5166c9be13b0
Suchith J N <suchithjn22@gmail.com>
2020-08-17 21:43:08 +0530

    address code review comments

--- content/en/docs/tasks/observability/metrics/querying-metrics/test.sh
+++ content/en/docs/tasks/observability/metrics/querying-metrics/test.sh
@@ -36,39 +36,34 @@ _set_ingress_environment_variables
 export INGRESS_URL="$INGRESS_HOST:$INGRESS_PORT"
 echo "$INGRESS_URL"
 for _ in {1..50}; do
-    curl -s http://"$INGRESS_URL"/productpage > /dev/null
+    curl -s "http://$INGRESS_URL/productpage" > /dev/null
 done
 
 # Now check Prometheus dashboard for the metric. It should be present
 function urlencode() {
-    local value=$1
-    python3 -c "import urllib.parse; print(urllib.parse.quote('''$value'''))"
+    python3 -c "import urllib.parse; print(urllib.parse.quote('''$1'''))"
 }
 
 function query_prometheus() {
-    local query_expr=$1
-    local prometheus_api_root='localhost:9090/api/v1'
-    local encoded_query=$(urlencode "$query_expr")
-    curl -sg -m 3.0 http://"$prometheus_api_root"/query?query="$encoded_query"
+    local prometheus_api_root="localhost:9090/api/v1"
+    local encoded_query=$(urlencode "$1")
+    curl -sg -m 3.0 "http://$prometheus_api_root/query?query=$encoded_query"
 }
 
 function query_total_requests() {
-    query_prometheus 'istio_requests_total' | jq .data.result[0].metric.__name__
+    query_prometheus "$snip_querying_istio_metrics_4" | jq '.data.result[0].metric.__name__'
 }
 
 function query_requests_to_productpage() {
-    local query='istio_requests_total{destination_service="productpage.default.svc.cluster.local"}'
-    query_prometheus "$query" | jq .data.result[0].metric.destination_service
+    query_prometheus "$snip_querying_istio_metrics_5" | jq '.data.result[0].metric.destination_service'
 }
 
 function query_requests_to_reviews_v3() {
-    local query='istio_requests_total{destination_service="reviews.default.svc.cluster.local",destination_version="v3"}'
-    query_prometheus "$query" | jq '.data.result[0].metric.destination_service,.data.result[0].metric.destination_version'
+    query_prometheus "$snip_querying_istio_metrics_6" | jq '.data.result[0].metric.destination_service,.data.result[0].metric.destination_version'
 }
 
 function query_rate_of_requests_to_productpage_5m() {
-    local query='rate(istio_requests_total{destination_service=~"productpage.*",response_code="200"}[5m])'
-    query_prometheus "$query" | jq .data.result[0].metric.destination_service
+    query_prometheus "$snip_querying_istio_metrics_7" | jq '.data.result[0].metric.destination_service'
 }
 
 # Prometheus living inside cluster is not accessible from outside.
@@ -78,8 +73,8 @@ istioctl dashboard prometheus &
 _verify_contains query_total_requests '"istio_requests_total"'
 _verify_contains query_requests_to_productpage '"productpage.default.svc.cluster.local"'
 _verify_contains query_requests_to_reviews_v3 '"reviews.default.svc.cluster.local"'
-# _verify_contains query_requests_to_reviews_v3 '"v3"'
-# _verify_contains query_rate_of_requests_to_productpage_5m '"productpage.default.svc.cluster.local"'
+_verify_contains query_requests_to_reviews_v3 '"v3"'
+_verify_contains query_rate_of_requests_to_productpage_5m '"productpage.default.svc.cluster.local"'
 pgrep istioctl | xargs kill
 
 # @cleanup
