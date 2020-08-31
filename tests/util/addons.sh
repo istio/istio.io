8fc77bb405bc038f76da6b3b4db18e9a96165226
Suchith J N <suchithjn22@gmail.com>
2020-08-31 08:18:48 +0530

    refactor addons with a more flexible deploy/undeploy function and fix naming of functions to access addons

--- tests/util/addons.sh
+++ tests/util/addons.sh
@@ -19,31 +19,59 @@ GRAFANA_MANIFEST_URL="https://raw.githubusercontent.com/istio/istio/master/sampl
 KIALI_MANIFEST_URL="https://raw.githubusercontent.com/istio/istio/master/samples/addons/kiali.yaml"
 PROMETHEUS_MANIFEST_URL="https://raw.githubusercontent.com/istio/istio/master/samples/addons/prometheus.yaml"
 ZIPKIN_MANIFEST_URL="https://raw.githubusercontent.com/istio/istio/master/samples/addons/extras/zipkin.yaml"
+JAEGER_MANIFEST_URL="https://raw.githubusercontent.com/istio/istio/master/samples/addons/jaeger.yaml"
 
-function _deploy_addons() {
-  kubectl apply -f "$GRAFANA_MANIFEST_URL"
-  kubectl apply -f "$KIALI_MANIFEST_URL"
-  kubectl apply -f "$PROMETHEUS_MANIFEST_URL"
-  kubectl apply -f "$ZIPKIN_MANIFEST_URL"
-}
-
-function _undeploy_addons() {
-  kubectl delete -f "$GRAFANA_MANIFEST_URL"
-  kubectl delete -f "$KIALI_MANIFEST_URL"
-  kubectl delete -f "$PROMETHEUS_MANIFEST_URL"
-  kubectl delete -f "$ZIPKIN_MANIFEST_URL"
-}
-
-function _wait_for_addon_deployment() {
-  local addon_list=( grafana kiali prometheus zipkin )
-  for addon in "${addon_list[@]}"; do
-    _wait_for_deployment istio-system "$addon"
+# Deploy the addons specified and wait for the deployment to complete. Currently
+# Zipkin, Jaeger, Grafana, Kiali and Prometheus are supported.
+function _deploy_and_wait_for_addons() {
+  for arg in "$@"; do
+    case "$arg" in
+    zipkin)     kubectl apply -f "$ZIPKIN_MANIFEST_URL"
+                 _wait_for_deployment istio-system zipkin
+                 ;;
+    grafana)    kubectl apply -f "$GRAFANA_MANIFEST_URL"
+                 _wait_for_deployment istio-system grafana
+                 ;;
+    jaeger)     kubectl apply -f "$JAEGER_MANIFEST_URL"
+                 _wait_for_deployment istio-system jaeger
+                 ;;
+    kiali)      kubectl apply -f "$KIALI_MANIFEST_URL"
+                 _wait_for_deployment istio-system kiali
+                 ;;
+    prometheus) kubectl apply -f "$PROMETHEUS_MANIFEST_URL"
+                 _wait_for_deployment istio-system prometheus
+                 ;;
+    *)           echo "unknown parameter $arg"
+                 exit 1
+    esac
   done
 }
 
-function _wait_for_config_distribution() {
-  local addon_list=( grafana kiali prometheus tracing )
-  for addon in "${addon_list[@]}"; do
+# Delete Istio addon deployments. Usually this is meant to be used
+# with _deploy_and_wait_for_addons function
+function _undeploy_addons() {
+  for arg in "$@"; do
+    case "$arg" in
+    zipkin)      kubectl delete -f "$ZIPKIN_MANIFEST_URL"
+                  ;;
+    grafana)     kubectl delete -f "$GRAFANA_MANIFEST_URL"
+                  ;;
+    jaeger)      kubectl delete -f "$JAEGER_MANIFEST_URL"
+                  ;;
+    kiali)       kubectl delete -f "$KIALI_MANIFEST_URL"
+                  ;;
+    prometheus)  kubectl delete -f "$PROMETHEUS_MANIFEST_URL"
+                  ;;
+    *)            echo "unknown parameter $arg"
+                  exit 1
+    esac
+  done
+}
+
+# Wait for the distribution of Gateway, VirtualService and DestinationRule
+# configuration to sidecar proxies
+function _wait_for_addon_config_distribution() {
+  for addon in "$@"; do
     _wait_for_istio Gateway istio-system "$addon-gateway"
     _wait_for_istio VirtualService istio-system "$addon-vs"
     _wait_for_istio DestinationRule istio-system "$addon"
