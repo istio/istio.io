c95d4b9278209c04bc46ccd5bdfe4f342939c308
Suchith J N <suchithjn22@gmail.com>
2020-09-01 14:12:48 +0530

    fix output snippets and add verifications

--- content/en/docs/tasks/observability/gateways/insecure_access_test.sh
+++ content/en/docs/tasks/observability/gateways/insecure_access_test.sh
@@ -59,9 +59,9 @@ _verify_same insecure_access_grafana "200"
 _verify_same insecure_access_tracing "200"
 
 # @cleanup
-set +e
-snip_cleanup_1
-snip_cleanup_2
-snip_cleanup_3
+_verify_same snip_cleanup_1 "$snip_cleanup_1_out" 
+_verify_same snip_cleanup_2 "$snip_cleanup_2_out"
+_verify_same snip_cleanup_3 "$snip_cleanup_3_out"
 
+set +e
 _undeploy_addons kiali prometheus grafana zipkin
