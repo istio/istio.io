bc1608421c303c22112f199ebffb4e99b3cae7d1
Eric Van Norman <ericvn@us.ibm.com>
2020-09-11 14:52:18 -0500

    Create split targets for tests by profile

--- tests/tests.mk
+++ tests/tests.mk
@@ -9,6 +9,30 @@ doc.test: init | $(JUNIT_REPORT)
 		-istio.test.tag=$(TAG) \
 		2>&1 | tee >($(JUNIT_REPORT) > $(JUNIT_OUT))
 
+doc.test.default: init | $(JUNIT_REPORT)
+	@${GO} test ${REPO_ROOT}/tests/setup/profile_default... \
+		-v -timeout=${TIMEOUT} -count=1 -p=1 \
+		-istio.test.hub=$(HUB) \
+		-istio.test.tag=$(TAG) \
+		-istio.test.kube.helm.values=meshConfig.defaultConfig.proxyMetadata.ISTIO_META_DNS_CAPTURE="" \
+		2>&1 | tee >($(JUNIT_REPORT) > $(JUNIT_OUT))
+
+doc.test.demo: init | $(JUNIT_REPORT)
+	@${GO} test ${REPO_ROOT}/tests/setup/profile_demo... \
+		-v -timeout=${TIMEOUT} -count=1 -p=1 \
+		-istio.test.hub=$(HUB) \
+		-istio.test.tag=$(TAG) \
+		-istio.test.kube.helm.values=meshConfig.defaultConfig.proxyMetadata.ISTIO_META_DNS_CAPTURE="" \
+		2>&1 | tee >($(JUNIT_REPORT) > $(JUNIT_OUT))
+
+doc.test.none: init | $(JUNIT_REPORT)
+	@${GO} test ${REPO_ROOT}/tests/setup/profile_none... \
+		-v -timeout=${TIMEOUT} -count=1 -p=1 \
+		-istio.test.hub=$(HUB) \
+		-istio.test.tag=$(TAG) \
+		-istio.test.kube.helm.values=meshConfig.defaultConfig.proxyMetadata.ISTIO_META_DNS_CAPTURE="" \
+		2>&1 | tee >($(JUNIT_REPORT) > $(JUNIT_OUT))
+
 doc.test.help:
 	@echo "The command \"make doc.test\" accepts three optional environment variables."
 	@echo -e "TEST: \n\tSpecify the test(s) to run using the directory path relative to content/en/docs. Default is all."
