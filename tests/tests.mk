33859cf75f0810fe790e415ec875ee91f69d85cd
Nathan Mittler <nmittler@gmail.com>
2020-09-11 15:06:21 -0700

    Adding test targets for each profile (#8131)
    * Adding test targets for each profile
    
    Also bumping up timeout slightly as a temporary fix for flakes.
    
    * addressing comments

--- tests/tests.mk
+++ tests/tests.mk
@@ -1,4 +1,14 @@
-export TIMEOUT ?= 30m
+DEFAULT_TIMEOUT=60m
+export TIMEOUT ?= ${DEFAULT_TIMEOUT}
+
+# gocache disabled by -count=1
+# tests in different packages forced to be sequential by -p=1
+doc.test.%: init | $(JUNIT_REPORT)
+	@${GO} test ${REPO_ROOT}/tests/setup/$*/... \
+		-v -timeout=${TIMEOUT} -count=1 -p=1 \
+		-istio.test.hub=$(HUB) \
+		-istio.test.tag=$(TAG) \
+		2>&1 | tee >($(JUNIT_REPORT) > $(JUNIT_OUT))
 
 # gocache disabled by -count=1
 # tests in different packages forced to be sequential by -p=1
@@ -13,5 +23,5 @@ doc.test.help:
 	@echo "The command \"make doc.test\" accepts three optional environment variables."
 	@echo -e "TEST: \n\tSpecify the test(s) to run using the directory path relative to content/en/docs. Default is all."
 	@echo -e "\tMultiple test names can be specified by separating them by commas."
-	@echo -e "TIMEOUT: \n\tSet the time limit exceeding which all tests will halt. Default is 30m."
+	@echo -e "TIMEOUT: \n\tSet the time limit exceeding which all tests will halt. Default is ${DEFAULT_TIMEOUT}."
 	@echo -e "Example: \n\tmake doc.test TEST=tasks/traffic-management TIMEOUT=1h"
