daeceef36968f5f9397269b40d3fc4fee41c4bcf
Eric Van Norman <ericvn@us.ibm.com>
2020-10-14 08:23:01 -0500

    Update istio/istio test reference (#8290)
    * Update istio/istio test reference
    
    * Update istioctl analyzer test as namespace message is now Info.
    
    * Review comment

--- content/en/docs/ops/diagnostic-tools/istioctl-analyze/snips.sh
+++ content/en/docs/ops/diagnostic-tools/istioctl-analyze/snips.sh
@@ -25,7 +25,7 @@ istioctl analyze --all-namespaces
 }
 
 ! read -r -d '' snip_analyze_all_namespace_sample_response <<\ENDSNIP
-Warning [IST0102] (Namespace default) The namespace is not enabled for Istio injection. Run 'kubectl label namespace default istio-injection=enabled' to enable it, or 'kubectl label namespace default istio-injection=disabled' to explicitly mark it as not needing injection
+Info [IST0102] (Namespace default) The namespace is not enabled for Istio injection. Run 'kubectl label namespace default istio-injection=enabled' to enable it, or 'kubectl label namespace default istio-injection=disabled' to explicitly mark it as not needing injection.
 ENDSNIP
 
 snip_fix_default_namespace() {
@@ -89,9 +89,7 @@ istioctl analyze -k --namespace frod
 }
 
 ! read -r -d '' snip_analyze_k_frod_out <<\ENDSNIP
-Warning [IST0102] (Namespace frod) The namespace is not enabled for Istio injection. Run 'kubectl label namespace frod istio-injection=enabled' to enable it, or 'kubectl label namespace frod istio-injection=disabled' to explicitly mark it as not needing injection
-Error: Analyzers found issues when analyzing namespace: frod.
-See https://istio.io/v1.8/docs/reference/config/analysis for more information about causes and resolutions.
+Info [IST0102] (Namespace frod) The namespace is not enabled for Istio injection. Run 'kubectl label namespace frod istio-injection=enabled' to enable it, or 'kubectl label namespace frod istio-injection=disabled' to explicitly mark it as not needing injection.
 ENDSNIP
 
 snip_analyze_suppress0102() {
