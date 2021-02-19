e7a7854f8de7bbf9df56dbba0cabfb3e4b4e95fb
Agata Stawarz-Pastewska <agatastpa@gmail.com>
2021-02-19 16:43:56 +0100

    Add FAQ page

--- scripts/gen_site.sh
+++ scripts/gen_site.sh
@@ -42,6 +42,7 @@ babel --source-maps --minified --no-comments --presets minify \
   tmp/js/lang.js \
   tmp/js/callToAction.js \
   tmp/js/events.js \
+   tmp/js/faq.js \
   --out-file generated/js/all.min.js
 
 babel --source-maps --minified --no-comments --presets minify \
