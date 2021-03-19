854a2219ff57de90cd0e429ef7e1841c5f907792
Jakub-Sadowski <jakubsadowski19@gmail.com>
2021-03-19 18:12:15 +0100

    Polidea update - tables, header fixes (#9318)
    * table fix, header shadow
    
    * fix code break
    
    * fix code break
    
    * add new line
    
    * fix spacing

--- src/ts/header.ts
+++ src/ts/header.ts
@@ -67,7 +67,7 @@ function handleHeader(): void {
     function toggleActiveHeader():void {
         const top = window.scrollY;
 
-        if(top >= 100) {
+        if(top >= 10) {
             header?.classList.add("active");
         } else {
             header?.classList.remove("active");
