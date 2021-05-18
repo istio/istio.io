d78bc47d7d3bbdeab07cdadadbd73e288e260a81
Eric Van Norman <ericvn@us.ibm.com>
2021-05-18 09:22:45 -0500

    Allow specification of additional conditional hosts

--- Makefile.overrides.mk
+++ Makefile.overrides.mk
@@ -18,4 +18,4 @@ CONTAINER_OPTIONS = -p 1313:1313 ${ADDITIONAL_CONTAINER_OPTIONS}
 # this repo is on the container plan by default
 BUILD_WITH_CONTAINER ?= 1
 
-CONDITIONAL_HOST_MOUNTS = --mount type=bind,source=/tmp,destination=/tmp ${}
+CONDITIONAL_HOST_MOUNTS = --mount type=bind,source=/tmp,destination=/tmp ${ADDITIONAL_CONDITIONAL_HOST_MOUNTS}
