39b2104d0c7b4abeec279ac32b77a297eca6f4c9
Eric Van Norman <ericvn@us.ibm.com>
2021-05-19 08:19:15 -0500

    Restore external authorization tests

--- content/en/docs/tasks/security/authorization/authz-custom/test.sh
+++ content/en/docs/tasks/security/authorization/authz-custom/test.sh
@@ -51,18 +51,19 @@ snip_define_the_external_authorizer_4
 _wait_for_deployment istio-system istiod
 
 # create the authorization policy and verify the ext-authz response.
+echo "Before enable"
 snip_enable_with_external_authorization_1
+echo "After enable"
 
-# Comment out the next lines as the tests seems to be failing. Release blocker issue created for this test: https://github.com/istio/istio/issues/32926
-#_verify_same snip_enable_with_external_authorization_2 "$snip_enable_with_external_authorization_2_out"
-#_verify_lines snip_enable_with_external_authorization_3 "
-#+ \"X-Ext-Authz-Check-Result\": \"allowed\",
-#"
+_verify_same snip_enable_with_external_authorization_2 "$snip_enable_with_external_authorization_2_out"
+_verify_lines snip_enable_with_external_authorization_3 "
++ \"X-Ext-Authz-Check-Result\": \"allowed\",
+"
 _verify_same snip_enable_with_external_authorization_4 "$snip_enable_with_external_authorization_4_out"
-#_verify_lines snip_enable_with_external_authorization_5 "
-#+ [gRPCv3][allowed]
-#+ [gRPCv3][denied]
-#"
+_verify_lines snip_enable_with_external_authorization_5 "
++ [gRPCv3][allowed]
++ [gRPCv3][denied]
+"
 
 # @cleanup
 snip_clean_up_1
--- go.mod
+++ go.mod
@@ -27,8 +27,8 @@ require (
 	github.com/pmezard/go-difflib v1.0.0
 	istio.io/client-go v1.10.0-rc.1.0.20210512214749-e6f28edf3fce // indirect
 	istio.io/gogo-genproto v0.0.0-20210511212328-954676fb66ee // indirect
-	istio.io/istio v0.0.0-20210518140137-7a4c1a8db702
-	istio.io/pkg v0.0.0-20210511212328-a723cf65d468
+	istio.io/istio v0.0.0-20210519112312-cdaf3848a0e7
+	istio.io/pkg v0.0.0-20210519012641-d57c32f9ee9c
 	k8s.io/apimachinery v0.21.0
 	k8s.io/client-go v0.21.0
 )
