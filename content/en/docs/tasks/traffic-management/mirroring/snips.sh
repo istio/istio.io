9fb86a1d73a991f5ac71e8271706d4f218a12d73
Istio Automation <istio-testing-bot@google.com>
2021-06-07 14:32:06 -0700

    Fix image used by sleep sample (doesn't use istio/istio sleep) (#9896)
    Co-authored-by: Eric Van Norman <ericvn@us.ibm.com>

--- content/en/docs/tasks/traffic-management/mirroring/snips.sh
+++ content/en/docs/tasks/traffic-management/mirroring/snips.sh
@@ -112,8 +112,8 @@ spec:
     spec:
       containers:
       - name: sleep
-        image: tutum/curl
-        command: ["/bin/sleep","infinity"]
+        image: curlimages/curl
+        command: ["/bin/sleep","3650d"]
         imagePullPolicy: IfNotPresent
 EOF
 }
