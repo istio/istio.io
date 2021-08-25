30540d6763718cee9a7fa2c56b75455895849a86
Eric Van Norman <ericvn@us.ibm.com>
2021-08-25 13:43:12 -0500

    Update VM test to a later base image.

--- content/en/docs/setup/install/virtual-machine/common.sh
+++ content/en/docs/setup/install/virtual-machine/common.sh
@@ -47,7 +47,7 @@ function setup_vm() {
     -v "${WORK_DIR}:/root" -v "${PWD}/content/en/docs/setup/install/virtual-machine:/test" \
     ${EXTRA_VM_ARGS:-} \
     -w "/root" \
-    gcr.io/istio-release/base:1.9-dev.2
+    gcr.io/istio-release/base:1.11-dev.6
 
   POD_CIDR=$(kubectl get node -ojsonpath='{.items[0].spec.podCIDR}')
   DOCKER_IP=$(docker inspect -f "{{ .NetworkSettings.Networks.kind.IPAddress }}" istio-testing-control-plane)
