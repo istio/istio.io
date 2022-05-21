e6d97ca4651de8a68d13fc2a25e88015ece03ceb
Eric Van Norman <ericvn@us.ibm.com>
2022-05-20 20:44:43 -0500

    Update common-files and fix lint

--- common/scripts/setup_env.sh
+++ common/scripts/setup_env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# shellcheck disable=SC2034
+# shellcheck disable=SC2034,SC2154
 
 # WARNING: DO NOT EDIT, THIS FILE IS PROBABLY A COPY
 #
@@ -73,7 +73,7 @@ fi
 
 # Build image to use
 if [[ "${IMAGE_VERSION:-}" == "" ]]; then
-  IMAGE_VERSION=master-2022-05-20T15-17-27
+  IMAGE_VERSION=master-2022-05-20T20-45-15
 fi
 if [[ "${IMAGE_NAME:-}" == "" ]]; then
   IMAGE_NAME=build-tools
