0a65d851505d680e6bf7ff4b4776ff344ad31ebf
John Howard <howardjohn@google.com>
2022-06-02 08:51:38 -0700

    Add back single wildcard

--- content/en/docs/tasks/traffic-management/egress/wildcard-egress-hosts/snips.sh
+++ content/en/docs/tasks/traffic-management/egress/wildcard-egress-hosts/snips.sh
@@ -65,6 +65,114 @@ snip_cleanup_direct_traffic_to_a_wildcard_host_1() {
 kubectl delete serviceentry wikipedia
 }
 
+snip_wildcard_configuration_for_a_single_hosting_server_1() {
+kubectl apply -f - <<EOF
+apiVersion: networking.istio.io/v1alpha3
+kind: Gateway
+metadata:
+  name: istio-egressgateway
+spec:
+  selector:
+    istio: egressgateway
+  servers:
+  - port:
+      number: 443
+      name: https
+      protocol: HTTPS
+    hosts:
+    - "*.wikipedia.org"
+    tls:
+      mode: PASSTHROUGH
+---
+apiVersion: networking.istio.io/v1alpha3
+kind: DestinationRule
+metadata:
+  name: egressgateway-for-wikipedia
+spec:
+  host: istio-egressgateway.istio-system.svc.cluster.local
+  subsets:
+    - name: wikipedia
+---
+apiVersion: networking.istio.io/v1alpha3
+kind: VirtualService
+metadata:
+  name: direct-wikipedia-through-egress-gateway
+spec:
+  hosts:
+  - "*.wikipedia.org"
+  gateways:
+  - mesh
+  - istio-egressgateway
+  tls:
+  - match:
+    - gateways:
+      - mesh
+      port: 443
+      sniHosts:
+      - "*.wikipedia.org"
+    route:
+    - destination:
+        host: istio-egressgateway.istio-system.svc.cluster.local
+        subset: wikipedia
+        port:
+          number: 443
+      weight: 100
+  - match:
+    - gateways:
+      - istio-egressgateway
+      port: 443
+      sniHosts:
+      - "*.wikipedia.org"
+    route:
+    - destination:
+        host: www.wikipedia.org
+        port:
+          number: 443
+      weight: 100
+EOF
+}
+
+snip_wildcard_configuration_for_a_single_hosting_server_2() {
+kubectl apply -f - <<EOF
+apiVersion: networking.istio.io/v1alpha3
+kind: ServiceEntry
+metadata:
+  name: www-wikipedia
+spec:
+  hosts:
+  - www.wikipedia.org
+  ports:
+  - number: 443
+    name: https
+    protocol: HTTPS
+  resolution: DNS
+EOF
+}
+
+snip_wildcard_configuration_for_a_single_hosting_server_3() {
+kubectl exec "$SOURCE_POD" -c sleep -- sh -c 'curl -s https://en.wikipedia.org/wiki/Main_Page | grep -o "<title>.*</title>"; curl -s https://de.wikipedia.org/wiki/Wikipedia:Hauptseite | grep -o "<title>.*</title>"'
+}
+
+! read -r -d '' snip_wildcard_configuration_for_a_single_hosting_server_3_out <<\ENDSNIP
+<title>Wikipedia, the free encyclopedia</title>
+<title>Wikipedia – Die freie Enzyklopädie</title>
+ENDSNIP
+
+snip_wildcard_configuration_for_a_single_hosting_server_4() {
+kubectl exec "$(kubectl get pod -l istio=egressgateway -n istio-system -o jsonpath='{.items[0].metadata.name}')" -c istio-proxy -n istio-system -- pilot-agent request GET clusters | grep '^outbound|443||www.wikipedia.org.*cx_total:'
+}
+
+! read -r -d '' snip_wildcard_configuration_for_a_single_hosting_server_4_out <<\ENDSNIP
+outbound|443||www.wikipedia.org::208.80.154.224:443::cx_total::2
+ENDSNIP
+
+snip_cleanup_wildcard_configuration_for_a_single_hosting_server_1() {
+kubectl delete serviceentry www-wikipedia
+kubectl delete gateway istio-egressgateway
+kubectl delete virtualservice direct-wikipedia-through-egress-gateway
+kubectl delete destinationrule egressgateway-for-wikipedia
+}
+
 snip_cleanup_1() {
 kubectl delete -f samples/sleep/sleep.yaml
 }
