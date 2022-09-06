0375f02eb79b5875fcff77080bc6d0a64dda8115
Istio Automation <istio-testing-bot@google.com>
2022-09-06 14:10:33 -0700

    egress tls origination - clarify required RBAC (#11816)
    Signed-off-by: Faseela K <faseela.k@est.tech>
    
    Signed-off-by: Faseela K <faseela.k@est.tech>
    Co-authored-by: Faseela K <faseela.k@est.tech>

--- content/en/docs/tasks/traffic-management/egress/egress-tls-origination/snips.sh
+++ content/en/docs/tasks/traffic-management/egress/egress-tls-origination/snips.sh
@@ -129,7 +129,7 @@ kubectl create secret generic client-credential --from-file=tls.key=client.examp
 }
 
 snip_configure_the_client_sleep_pod_2() {
-kubectl create role client-credential-role --resource=secret --verb=get,list,watch
+kubectl create role client-credential-role --resource=secret --verb=list
 kubectl create rolebinding client-credential-role-binding --role=client-credential-role --serviceaccount=default:sleep
 }
 
