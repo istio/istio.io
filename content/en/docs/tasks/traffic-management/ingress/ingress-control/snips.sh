a9b49e54f8231bda8aba09a1141b122841373e5b
Eric Van Norman <ericvn@us.ibm.com>
2022-09-19 13:46:17 -0500

    Run make gen

--- content/en/docs/tasks/traffic-management/ingress/ingress-control/snips.sh
+++ content/en/docs/tasks/traffic-management/ingress/ingress-control/snips.sh
@@ -38,7 +38,6 @@ snip_determining_the_ingress_ip_and_ports_3() {
 export INGRESS_HOST=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
 export INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="http2")].port}')
 export SECURE_INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="https")].port}')
-export TCP_INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="tcp")].port}')
 }
 
 snip_determining_the_ingress_ip_and_ports_4() {
@@ -48,11 +47,10 @@ export INGRESS_HOST=$(kubectl -n istio-system get service istio-ingressgateway -
 snip_determining_the_ingress_ip_and_ports_5() {
 export INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="http2")].nodePort}')
 export SECURE_INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="https")].nodePort}')
-export TCP_INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="tcp")].nodePort}')
 }
 
 snip_determining_the_ingress_ip_and_ports_6() {
-export INGRESS_HOST=worker-node-address
+export INGRESS_HOST=workerNodeAddress
 }
 
 snip_determining_the_ingress_ip_and_ports_7() {
