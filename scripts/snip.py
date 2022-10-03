9b9202c7ec935401dd28e7efa4d455e6d36dc63c
Eric Van Norman <ericvn@us.ibm.com>
2022-10-03 16:59:32 -0500

    Remove k8s_gateway_api_type

--- scripts/snip.py
+++ scripts/snip.py
@@ -90,7 +90,6 @@ try:
     istio_version = docs_config['version']
     istio_full_version = docs_config['full_version']
     k8s_gateway_api_version = docs_config['k8s_gateway_api_version']
-    k8s_gateway_api_type = docs_config['k8s_gateway_api_type']
 except:
     sys.stderr.write('failed to retrieve data from "data/args.yml"\n')
     sys.exit(1)
@@ -179,7 +178,6 @@ with open(markdown, 'rt', encoding='utf-8') as mdfile:
                 line = line.replace("{{< istio_version >}}", istio_version)
                 line = line.replace("{{< istio_full_version >}}", istio_full_version)
                 line = line.replace("{{< k8s_gateway_api_version >}}", k8s_gateway_api_version)
-                line = line.replace("{{< k8s_gateway_api_type >}}", k8s_gateway_api_type)
                 current_snip["script"].append(line)
 
 if len(boilerplates) > 0:
