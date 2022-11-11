4703cfaad5761fa68fe72ccc803ae266a5e92a71
Frank Budinsky <frankb@ca.ibm.com>
2022-11-11 13:52:54 -0500

    gateway-api: add instructions to mirroring task

--- content/en/docs/tasks/traffic-management/mirroring/test.sh
+++ content/en/docs/tasks/traffic-management/mirroring/test.sh
@@ -21,6 +21,8 @@ set -e
 set -u
 set -o pipefail
 
+GATEWAY_API="${GATEWAY_API:-false}"
+
 # @setup profile=default
 
 kubectl label namespace default istio-injection- --overwrite
@@ -38,35 +40,45 @@ _wait_for_deployment default httpbin-v1
 _wait_for_deployment default httpbin-v2
 _wait_for_deployment default sleep
 
-snip_creating_a_default_routing_policy_1
+if [ "$GATEWAY_API" == "true" ]; then
+    snip_creating_a_default_routing_policy_2
+else
+    snip_creating_a_default_routing_policy_1
 
-# wait for config
-_wait_for_istio virtualservice default httpbin
-_wait_for_istio destinationrule default httpbin
+    # wait for config
+    _wait_for_istio virtualservice default httpbin
+    _wait_for_istio destinationrule default httpbin
+fi
 
-_verify_contains snip_creating_a_default_routing_policy_2 "headers"
+_verify_contains snip_creating_a_default_routing_policy_3 "headers"
 
-_verify_contains snip_creating_a_default_routing_policy_3 "GET /headers HTTP/1.1"
+_verify_contains snip_creating_a_default_routing_policy_4 "GET /headers HTTP/1.1"
 
-_verify_not_contains snip_creating_a_default_routing_policy_4 "GET /headers HTTP/1.1"
+_verify_not_contains snip_creating_a_default_routing_policy_5 "GET /headers HTTP/1.1"
 
-snip_mirroring_traffic_to_v2_1
+if [ "$GATEWAY_API" == "true" ]; then
+    snip_mirroring_traffic_to_v2_2
+else
+    snip_mirroring_traffic_to_v2_1
 
-# wait for config
-_wait_for_istio virtualservice default httpbin
+    # wait for config
+    _wait_for_istio virtualservice default httpbin
+fi
 
 # Set environment variables. TODO: why didn't the exports from snip_creating_a_default_routing_policy_2/3/4 take?
 export SLEEP_POD=$(kubectl get pod -l app=sleep -o jsonpath={.items..metadata.name})
 export V1_POD=$(kubectl get pod -l app=httpbin,version=v1 -o jsonpath={.items..metadata.name})
 export V2_POD=$(kubectl get pod -l app=httpbin,version=v2 -o jsonpath={.items..metadata.name})
 
-snip_mirroring_traffic_to_v2_2
+snip_mirroring_traffic_to_v2_3
 
 # TODO: This should check for 2 lines with the GET request
-_verify_contains snip_mirroring_traffic_to_v2_3 "GET /headers HTTP/1.1"
-
 _verify_contains snip_mirroring_traffic_to_v2_4 "GET /headers HTTP/1.1"
 
+_verify_contains snip_mirroring_traffic_to_v2_5 "GET /headers HTTP/1.1"
+
 # @cleanup
-snip_cleaning_up_1
-snip_cleaning_up_2
+if [ "$GATEWAY_API" != "true" ]; then
+    snip_cleaning_up_1
+    snip_cleaning_up_3
+fi
