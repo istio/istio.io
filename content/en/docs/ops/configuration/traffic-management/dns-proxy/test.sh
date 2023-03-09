165cb1ca10e09a42bdbf7f4ee8bc237c4e8f2e3c
Faseela K <faseela.k@est.tech>
2023-03-09 21:00:57 +0100

    Add tests for smart DNS docs
    Signed-off-by: Faseela K <faseela.k@est.tech>

---
+++ content/en/docs/ops/configuration/traffic-management/dns-proxy/test.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC1090,SC2154
+
+# Copyright Istio Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# @setup profile=none
+
+set -e
+set -u
+set -o pipefail
+
+source "tests/util/samples.sh"
+
+snip_getting_started_1
+snip_dns_capture_in_action_1
+_verify_first_line snip_dns_capture_in_action_2 "$snip_dns_capture_in_action_2_out"
+snip_address_auto_allocation_1
+_verify_first_line snip_address_auto_allocation_2 "$snip_address_auto_allocation_2_out"
+
+# @cleanup
+
+snip_cleanup_1
+
