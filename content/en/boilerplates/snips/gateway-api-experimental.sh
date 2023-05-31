163eb862d4eb4774269010e5c15dc97b9973ad41
Eric Van Norman <ericvn@us.ibm.com>
2023-05-31 12:29:32 -0500

    gatewayversion

--- content/en/boilerplates/snips/gateway-api-experimental.sh
+++ content/en/boilerplates/snips/gateway-api-experimental.sh
@@ -21,5 +21,5 @@
 ####################################################################################################
 
 bpsnip_gateway_api_experimental_install_experimental_crds() {
-kubectl kustomize "github.com/kubernetes-sigs/gateway-api/config/crd/experimental?ref=v0.7.1-0.20230517171234-6b9b7346ca5b" | kubectl apply -f -
+kubectl kustomize "github.com/kubernetes-sigs/gateway-api/config/crd/experimental?ref=6b9b7346ca5b54a50c3afb0a0573b16b1c84336a" | kubectl apply -f -
 }
