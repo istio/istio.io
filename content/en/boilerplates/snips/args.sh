104c7a8ef0ee5f968de06a29de30cf8ccd608273
Eric Van Norman <ericvn@us.ibm.com>
2023-06-07 09:53:28 -0500

    Master has a non release gateway API. Changes to allow that.

--- content/en/boilerplates/snips/args.sh
+++ content/en/boilerplates/snips/args.sh
@@ -21,7 +21,7 @@
 ####################################################################################################
 
 ! read -r -d '' bpsnip_args_gateway_api_version <<\ENDSNIP
-v0.7.1-0.20230517171234-6b9b7346ca5b
+6b9b7346ca5b54a50c3afb0a0573b16b1c84336a
 ENDSNIP
 
 ! read -r -d '' bpsnip_args_istio_previous_version <<\ENDSNIP
