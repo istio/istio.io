7b8d238c49ff520a16222db114c7b9707b269652
Frank Budinsky <frankb@ca.ibm.com>
2023-07-14 15:22:53 -0400

    fix test

--- content/en/docs/tasks/security/authentication/jwt-route/test.sh
+++ content/en/docs/tasks/security/authentication/jwt-route/test.sh
@@ -40,10 +40,17 @@ snip_configuring_ingress_routing_based_on_jwt_claims_2
 _verify_elided snip_validating_ingress_routing_based_on_jwt_claims_1 "$snip_validating_ingress_routing_based_on_jwt_claims_1_out"
 _verify_elided snip_validating_ingress_routing_based_on_jwt_claims_2 "$snip_validating_ingress_routing_based_on_jwt_claims_2_out"
 
+# Pull the Istio branch from the docs configuration file.
+ISTIO_BRANCH=$(yq '.source_branch_name' "${REPO_ROOT}"/data/args.yml)
+
 _verify_same snip_validating_ingress_routing_based_on_jwt_claims_3 "$snip_validating_ingress_routing_based_on_jwt_claims_3_out"
+TOKEN_GROUP_URL="https://raw.githubusercontent.com/istio/istio/${ISTIO_BRANCH}/security/tools/jwt/samples/groups-scope.jwt"
+TOKEN_GROUP=$(curl "${TOKEN_GROUP_URL}" -s)
 _verify_elided snip_validating_ingress_routing_based_on_jwt_claims_4 "$snip_validating_ingress_routing_based_on_jwt_claims_4_out"
 
 _verify_same snip_validating_ingress_routing_based_on_jwt_claims_5 "$snip_validating_ingress_routing_based_on_jwt_claims_5_out"
+TOKEN_NO_GROUP_URL="https://raw.githubusercontent.com/istio/istio/${ISTIO_BRANCH}/security/tools/jwt/samples/demo.jwt"
+TOKEN_NO_GROUP=$(curl "${TOKEN_NO_GROUP_URL}" -s)
 _verify_elided snip_validating_ingress_routing_based_on_jwt_claims_6 "$snip_validating_ingress_routing_based_on_jwt_claims_6_out"
 
 # @cleanup
