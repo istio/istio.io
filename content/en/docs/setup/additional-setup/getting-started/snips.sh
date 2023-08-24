cbac2c1c2254a02178df852ed5fe694a8e7aecef
Eric Van Norman <ericvn@us.ibm.com>
2023-08-24 16:31:00 +0000

    Update to latest istio commit. Also use rc for gateway-api.

--- content/en/docs/setup/additional-setup/getting-started/snips.sh
+++ content/en/docs/setup/additional-setup/getting-started/snips.sh
@@ -23,7 +23,7 @@ source "content/en/boilerplates/snips/trace-generation.sh"
 
 snip__1() {
 kubectl get crd gateways.gateway.networking.k8s.io &> /dev/null || \
-  { kubectl kustomize "github.com/kubernetes-sigs/gateway-api/config/crd?ref=v0.7.1" | kubectl apply -f -; }
+  { kubectl kustomize "github.com/kubernetes-sigs/gateway-api/config/crd?ref=v0.8.0-rc1" | kubectl apply -f -; }
 }
 
 snip_download_istio_1() {
