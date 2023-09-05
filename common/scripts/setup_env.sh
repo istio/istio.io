969ee2ff365d9f2b178d4b168c14821d337c1f2b
Eric Van Norman <ericvn@us.ibm.com>
2023-09-05 16:31:26 +0000

    create a new release branch for 1.19

--- common/scripts/setup_env.sh
+++ common/scripts/setup_env.sh
@@ -75,7 +75,7 @@ fi
 TOOLS_REGISTRY_PROVIDER=${TOOLS_REGISTRY_PROVIDER:-gcr.io}
 PROJECT_ID=${PROJECT_ID:-istio-testing}
 if [[ "${IMAGE_VERSION:-}" == "" ]]; then
-  IMAGE_VERSION=master-dfedc8d689da03c57793a8e62e75a6bc36d2ae07
+  IMAGE_VERSION=release-1.19-04ab00931b61c082300832a7dd51634e5e3634ad
 fi
 if [[ "${IMAGE_NAME:-}" == "" ]]; then
   IMAGE_NAME=build-tools
