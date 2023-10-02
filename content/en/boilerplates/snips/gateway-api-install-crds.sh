7ecbbbc7a48283349a0ca12e11ff77d63bc352d2
Eric Van Norman <ericvn@us.ibm.com>
2023-10-02 13:42:31 -0500

    [release-1.19] Update the istio test reference to the 1.19.1 candidate (#13938)
    * Update the istio test reference to the 1.19.1 candidate
    
    * Fix ops/configuration/traffic-management/dns-proxy/test.sh

--- content/en/boilerplates/snips/gateway-api-install-crds.sh
+++ content/en/boilerplates/snips/gateway-api-install-crds.sh
@@ -22,5 +22,5 @@
 
 bpsnip_gateway_api_install_crds_install_crds() {
 kubectl get crd gateways.gateway.networking.k8s.io &> /dev/null || \
-  { kubectl kustomize "github.com/kubernetes-sigs/gateway-api/config/crd?ref=v0.8.0-rc1" | kubectl apply -f -; }
+  { kubectl kustomize "github.com/kubernetes-sigs/gateway-api/config/crd?ref=v0.8.0" | kubectl apply -f -; }
 }
