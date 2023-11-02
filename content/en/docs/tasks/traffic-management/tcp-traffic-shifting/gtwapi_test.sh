4ecd05548bb84ce0aa84f3037ed0c904c4a39b67
Eric Van Norman <ericvn@us.ibm.com>
2023-11-02 20:53:09 +0000

    Move gateway tcp-traffic shifting to demo

--- content/en/docs/tasks/traffic-management/tcp-traffic-shifting/gtwapi_test.sh
+++ content/en/docs/tasks/traffic-management/tcp-traffic-shifting/gtwapi_test.sh
@@ -18,7 +18,7 @@
 source "tests/util/gateway-api.sh"
 install_gateway_api_crds
 
-# @setup profile=none
+# @setup profile=demo
 source "content/en/boilerplates/snips/gateway-api-gamma-support.sh"
 bpsnip_gateway_api_gamma_support_enable_alpha_crds
 source "content/en/docs/tasks/traffic-management/tcp-traffic-shifting/test.sh"
