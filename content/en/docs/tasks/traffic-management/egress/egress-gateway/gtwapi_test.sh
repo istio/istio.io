f632773c4250ef102bd240827248f9d04a30385c
Frank Budinsky <frankb@ca.ibm.com>
2024-01-16 14:47:40 -0500

    use profile none

--- content/en/docs/tasks/traffic-management/egress/egress-gateway/gtwapi_test.sh
+++ content/en/docs/tasks/traffic-management/egress/egress-gateway/gtwapi_test.sh
@@ -18,7 +18,10 @@
 source "tests/util/gateway-api.sh"
 install_gateway_api_crds
 
-# @setup profile=demo
+# @setup profile=none
+istioctl install --set profile=minimal --set values.pilot.env.PILOT_ENABLE_ALPHA_GATEWAY_API=true --set meshConfig.accessLogFile=/dev/stdout -y
+_wait_for_deployment istio-system istiod
+
 source "content/en/docs/tasks/traffic-management/egress/egress-gateway/test.sh"
 
 # @cleanup
@@ -26,4 +29,6 @@ snip_cleanup_http_gateway_2
 snip_cleanup_https_gateway_2
 snip_cleanup_network_policies_2
 snip_cleanup_1
+istioctl uninstall --purge
+kubectl delete ns istio-system
 remove_gateway_api_crds
