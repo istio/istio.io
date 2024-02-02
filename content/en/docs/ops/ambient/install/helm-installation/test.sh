71242483e1b532249b7bf951d20bdb3bc11ba8ae
Faseela K <faseela.k@est.tech>
2024-02-02 10:07:30 +0100

    small enhancements to ambient install tests
    Signed-off-by: Faseela K <faseela.k@est.tech>

--- content/en/docs/ops/ambient/install/helm-installation/test.sh
+++ content/en/docs/ops/ambient/install/helm-installation/test.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+# shellcheck disable=SC2154
+
 # Copyright Istio Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -35,10 +37,8 @@ _wait_for_daemonset istio-system ztunnel
 _rewrite_helm_repo snip_install_ingress
 _wait_for_deployment istio-ingress istio-ingress
 
-# shellcheck disable=SC2154
-_verify_contains snip_check_pods "istiod"
-_verify_contains snip_check_pods "istio-cni-node"
-_verify_contains snip_check_pods "ztunnel"
+_verify_like snip_show_components "$snip_show_components_out"
+_verify_like snip_check_pods "$snip_check_pods_out"
 
 # @cleanup
 snip_delete_cni
