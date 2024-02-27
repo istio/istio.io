0997aa87fcc983475662bde4dd27a37be23b6878
Frank Budinsky <frankb@ca.ibm.com>
2024-02-27 15:50:16 -0500

    gateway-api: add Gateway API instructions to wildcard egress doc (#14654)
    * gateway-api: add Gateway API instructions to wildcard egress doc
    
    * v1beta1
    
    * regen

--- content/en/docs/tasks/traffic-management/egress/wildcard-egress-hosts/snips.sh
+++ content/en/docs/tasks/traffic-management/egress/wildcard-egress-hosts/snips.sh
@@ -19,20 +19,28 @@
 # WARNING: THIS IS AN AUTO-GENERATED FILE, DO NOT EDIT. PLEASE MODIFY THE ORIGINAL MARKDOWN FILE:
 #          docs/tasks/traffic-management/egress/wildcard-egress-hosts/index.md
 ####################################################################################################
+source "content/en/boilerplates/snips/gateway-api-gamma-support.sh"
 
 snip_before_you_begin_1() {
 istioctl install --set values.pilot.env.PILOT_ENABLE_CONFIG_DISTRIBUTION_TRACKING=true --set profile=demo --set meshConfig.outboundTrafficPolicy.mode=REGISTRY_ONLY
 }
 
 snip_before_you_begin_2() {
-kubectl apply -f samples/sleep/sleep.yaml
+istioctl install --set values.pilot.env.PILOT_ENABLE_CONFIG_DISTRIBUTION_TRACKING=true --set profile=minimal -y \
+    --set values.pilot.env.PILOT_ENABLE_ALPHA_GATEWAY_API=true \
+    --set meshConfig.accessLogFile=/dev/stdout \
+    --set meshConfig.outboundTrafficPolicy.mode=REGISTRY_ONLY
 }
 
 snip_before_you_begin_3() {
-kubectl apply -f <(istioctl kube-inject -f samples/sleep/sleep.yaml)
+kubectl apply -f samples/sleep/sleep.yaml
 }
 
 snip_before_you_begin_4() {
+kubectl apply -f <(istioctl kube-inject -f samples/sleep/sleep.yaml)
+}
+
+snip_before_you_begin_5() {
 export SOURCE_POD=$(kubectl get pod -l app=sleep -o jsonpath={.items..metadata.name})
 }
 
@@ -134,6 +142,71 @@ EOF
 
 snip_configure_egress_gateway_traffic_to_a_wildcard_host_2() {
 kubectl apply -f - <<EOF
+apiVersion: gateway.networking.k8s.io/v1beta1
+kind: Gateway
+metadata:
+  name: wikipedia-egress-gateway
+  annotations:
+    networking.istio.io/service-type: ClusterIP
+spec:
+  gatewayClassName: istio
+  listeners:
+  - name: tls
+    hostname: "*.wikipedia.org"
+    port: 443
+    protocol: TLS
+    tls:
+      mode: Passthrough
+    allowedRoutes:
+      namespaces:
+        from: Same
+---
+apiVersion: gateway.networking.k8s.io/v1alpha2
+kind: TLSRoute
+metadata:
+  name: direct-wikipedia-to-egress-gateway
+spec:
+  parentRefs:
+  - kind: ServiceEntry
+    group: networking.istio.io
+    name: wikipedia
+  rules:
+  - backendRefs:
+    - name: wikipedia-egress-gateway-istio
+      port: 443
+---
+apiVersion: gateway.networking.k8s.io/v1alpha2
+kind: TLSRoute
+metadata:
+  name: forward-wikipedia-from-egress-gateway
+spec:
+  parentRefs:
+  - name: wikipedia-egress-gateway
+  hostnames:
+  - "*.wikipedia.org"
+  rules:
+  - backendRefs:
+    - kind: Hostname
+      group: networking.istio.io
+      name: www.wikipedia.org
+      port: 443
+---
+apiVersion: networking.istio.io/v1alpha3
+kind: ServiceEntry
+metadata:
+  name: wikipedia
+spec:
+  hosts:
+  - "*.wikipedia.org"
+  ports:
+  - number: 443
+    name: https
+    protocol: HTTPS
+EOF
+}
+
+snip_configure_egress_gateway_traffic_to_a_wildcard_host_3() {
+kubectl apply -f - <<EOF
 apiVersion: networking.istio.io/v1alpha3
 kind: ServiceEntry
 metadata:
@@ -149,20 +222,28 @@ spec:
 EOF
 }
 
-snip_configure_egress_gateway_traffic_to_a_wildcard_host_3() {
+snip_configure_egress_gateway_traffic_to_a_wildcard_host_4() {
 kubectl exec "$SOURCE_POD" -c sleep -- sh -c 'curl -s https://en.wikipedia.org/wiki/Main_Page | grep -o "<title>.*</title>"; curl -s https://de.wikipedia.org/wiki/Wikipedia:Hauptseite | grep -o "<title>.*</title>"'
 }
 
-! read -r -d '' snip_configure_egress_gateway_traffic_to_a_wildcard_host_3_out <<\ENDSNIP
+! read -r -d '' snip_configure_egress_gateway_traffic_to_a_wildcard_host_4_out <<\ENDSNIP
 <title>Wikipedia, the free encyclopedia</title>
 <title>Wikipedia – Die freie Enzyklopädie</title>
 ENDSNIP
 
-snip_configure_egress_gateway_traffic_to_a_wildcard_host_4() {
+snip_configure_egress_gateway_traffic_to_a_wildcard_host_5() {
 kubectl exec "$(kubectl get pod -l istio=egressgateway -n istio-system -o jsonpath='{.items[0].metadata.name}')" -c istio-proxy -n istio-system -- pilot-agent request GET clusters | grep '^outbound|443||www.wikipedia.org.*cx_total:'
 }
 
-! read -r -d '' snip_configure_egress_gateway_traffic_to_a_wildcard_host_4_out <<\ENDSNIP
+! read -r -d '' snip_configure_egress_gateway_traffic_to_a_wildcard_host_5_out <<\ENDSNIP
+outbound|443||www.wikipedia.org::208.80.154.224:443::cx_total::2
+ENDSNIP
+
+snip_configure_egress_gateway_traffic_to_a_wildcard_host_6() {
+kubectl exec "$(kubectl get pod -l gateway.networking.k8s.io/gateway-name=wikipedia-egress-gateway -o jsonpath='{.items[0].metadata.name}')" -c istio-proxy -- pilot-agent request GET clusters | grep '^outbound|443||www.wikipedia.org.*cx_total:'
+}
+
+! read -r -d '' snip_configure_egress_gateway_traffic_to_a_wildcard_host_6_out <<\ENDSNIP
 outbound|443||www.wikipedia.org::208.80.154.224:443::cx_total::2
 ENDSNIP
 
@@ -171,6 +252,14 @@ kubectl delete serviceentry www-wikipedia
 kubectl delete gateway istio-egressgateway
 kubectl delete virtualservice direct-wikipedia-through-egress-gateway
 kubectl delete destinationrule egressgateway-for-wikipedia
+}
+
+snip_cleanup_egress_gateway_traffic_to_a_wildcard_host_2() {
+kubectl delete se wikipedia
+kubectl delete se www-wikipedia
+kubectl delete gtw wikipedia-egress-gateway
+kubectl delete tlsroute direct-wikipedia-to-egress-gateway
+kubectl delete tlsroute forward-wikipedia-from-egress-gateway
 }
 
 snip_cleanup_1() {
