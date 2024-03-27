c10c8a8bcbc8bde4bab4ed6fec8ddf42562cecf4
Michael Ac <34135587+nauticalmike@users.noreply.github.com>
2024-03-27 18:00:28 -0400

    fixing tests

--- content/en/docs/ops/ambient/getting-started/snips.sh
+++ content/en/docs/ops/ambient/getting-started/snips.sh
@@ -146,7 +146,7 @@ kubectl logs ds/ztunnel -n istio-system | grep "starting proxy"
 }
 
 ! IFS=$'\n' read -r -d '' snip_adding_your_application_to_the_ambient_mesh_3_out <<\ENDSNIP
-received netns, starting proxy
+... received netns, starting proxy
 ENDSNIP
 
 snip_adding_your_application_to_the_ambient_mesh_4() {
