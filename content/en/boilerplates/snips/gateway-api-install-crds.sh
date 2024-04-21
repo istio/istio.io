f394a3d6496d8330913b47c7da11b123ebc6566b
istio-testing <istio-testing-bot@google.com>
2024-04-21 02:04:22 +0000

    Automator: update istio@ test reference

--- content/en/boilerplates/snips/gateway-api-install-crds.sh
+++ content/en/boilerplates/snips/gateway-api-install-crds.sh
@@ -22,5 +22,5 @@
 
 bpsnip_gateway_api_install_crds_install_crds() {
 kubectl get crd gateways.gateway.networking.k8s.io &> /dev/null || \
-  { kubectl kustomize "github.com/kubernetes-sigs/gateway-api/config/crd?ref=e8cdaaf9b4ff999d5d0320fbbed6135cd2cfd9d2" | kubectl apply -f -; }
+  { kubectl kustomize "github.com/kubernetes-sigs/gateway-api/config/crd?ref=7f9f51098f405344a3a7eb13c55895c77919f706" | kubectl apply -f -; }
 }
