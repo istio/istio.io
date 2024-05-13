8efec6a684d575eada157fb895101e77f0d6eedb
John Howard <john.howard@solo.io>
2024-05-13 15:21:28 -0700

    create a new release branch for 1.22

--- common/scripts/setup_env.sh
+++ common/scripts/setup_env.sh
@@ -75,7 +75,7 @@ fi
 TOOLS_REGISTRY_PROVIDER=${TOOLS_REGISTRY_PROVIDER:-gcr.io}
 PROJECT_ID=${PROJECT_ID:-istio-testing}
 if [[ "${IMAGE_VERSION:-}" == "" ]]; then
-  IMAGE_VERSION=master-5febdc9ef2a2e05a9451ced0db096b4ecefd2a64
+  IMAGE_VERSION=release-1.22-90c1573ac8a673ef69c7d0587232efa748243fac
 fi
 if [[ "${IMAGE_NAME:-}" == "" ]]; then
   IMAGE_NAME=build-tools
