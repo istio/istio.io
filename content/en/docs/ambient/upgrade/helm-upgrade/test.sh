54e174b6507f6e183978c05366512c6668410a3a
Mitch Cononrs <mitchconnors@gmail.com>
2024-07-12 09:36:28 -0700

    explicitly set tagname, as env vars don't carry over to cleanup

--- content/en/docs/ambient/upgrade/helm-upgrade/test.sh
+++ content/en/docs/ambient/upgrade/helm-upgrade/test.sh
@@ -23,15 +23,13 @@ source "content/en/docs/ambient/upgrade/helm-upgrade/common.sh"
 # @setup profile=none
 _install_istio_ambient_helm
 
-MYTAG=tagname
+export MYTAG=tagname
 
 snip_list_revisions
 snip_update_helm
 snip_istioctl_precheck
 snip_prerequisites_4
-snip_list_tags
-snip_upgrade_tag
-snip_rollback_tag
+
 
 _rewrite_helm_repo snip_upgrade_istiod
 _wait_for_deployment istio-system istiod-"$REVISION"
@@ -42,11 +40,14 @@ _wait_for_daemonset istio-system istio-cni-node
 _rewrite_helm_repo snip_upgrade_gateway
 _wait_for_deployment istio-ingress istio-ingress
 
+snip_list_tags
+_rewrite_helm_repo snip_upgrade_tag
+_rewrite_helm_repo snip_rollback_tag
 
 # @cleanup
 
 # upgrading a tag creates an MWC, let's clean it up 
 export REVISION=istio-1-22-1
-helm template istiod istio/istiod -s templates/revision-tags.yaml --set revisionTags="{$MYTAG}" --set revision="$REVISION" -n istio-system | kubectl delete -f -
+helm template istiod istio/istiod -s templates/revision-tags.yaml --set revisionTags="{tagname}" --set revision="$OLD_REVISION" -n istio-system | kubectl delete -f -
 helm delete istiod-"$REVISION" -n istio-system
 _remove_istio_ambient_helm
