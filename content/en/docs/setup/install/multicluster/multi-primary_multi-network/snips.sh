8d2a6e3e8363340d2d552d49a1da58524a24128a
nshankar <nshankar@microsoft.com>
2024-07-23 13:48:54 -0400

    Add primary-remote steps
    Signed-off-by: nshankar <nshankar@microsoft.com>

--- content/en/docs/setup/install/multicluster/multi-primary_multi-network/snips.sh
+++ content/en/docs/setup/install/multicluster/multi-primary_multi-network/snips.sh
@@ -44,12 +44,11 @@ istioctl install --context="${CTX_CLUSTER1}" -f cluster1.yaml
 }
 
 snip_configure_cluster1_as_a_primary_3() {
-kubectl create namespace istio-system
-helm install istio-base istio/base -n istio-system --kube-context $CTX_CLUSTER1 
+helm install istio-base istio/base -n istio-system --kube-context ${CTX_CLUSTER1} 
 }
 
 snip_configure_cluster1_as_a_primary_4() {
-helm install istiod istio/istiod -n istio-system --kube-context $CTX_CLUSTER1 --set global.meshID=mesh1 --set global.multiCluster.clusterName=cluster1 --set global.network=network1
+helm install istiod istio/istiod -n istio-system --kube-context ${CTX_CLUSTER1} --set global.meshID=mesh1 --set global.multiCluster.clusterName=cluster1 --set global.network=network1
 }
 
 snip_install_the_eastwest_gateway_in_cluster1_1() {
@@ -62,7 +61,7 @@ snip_install_the_eastwest_gateway_in_cluster1_2() {
 }
 
 ! IFS=$'\n' read -r -d '' snip_install_the_eastwest_gateway_in_cluster1_2_out <<\ENDSNIP
-helm install istio-eastwestgateway istio/gateway -n istio-system --kube-context $CTX_CLUSTER1 --set name=istio-eastwestgateway --set networkGateway=network1
+helm install istio-eastwestgateway istio/gateway -n istio-system --kube-context ${CTX_CLUSTER1} --set name=istio-eastwestgateway --set networkGateway=network1
 ENDSNIP
 
 snip_install_the_eastwest_gateway_in_cluster1_3() {
@@ -104,11 +103,11 @@ istioctl install --context="${CTX_CLUSTER2}" -f cluster2.yaml
 
 snip_configure_cluster2_as_a_primary_3() {
 kubectl create namespace istio-system
-helm install istio-base istio/base -n istio-system --kube-context $CTX_CLUSTER2
+helm install istio-base istio/base -n istio-system --kube-context ${CTX_CLUSTER2}
 }
 
 snip_configure_cluster2_as_a_primary_4() {
-helm install istiod istio/istiod -n istio-system --kube-context $CTX_CLUSTER2 --set global.meshID=mesh1 --set global.multiCluster.clusterName=cluster2 --set global.network=network2
+helm install istiod istio/istiod -n istio-system --kube-context ${CTX_CLUSTER2} --set global.meshID=mesh1 --set global.multiCluster.clusterName=cluster2 --set global.network=network2
 }
 
 snip_install_the_eastwest_gateway_in_cluster2_1() {
@@ -121,7 +120,7 @@ snip_install_the_eastwest_gateway_in_cluster2_2() {
 }
 
 ! IFS=$'\n' read -r -d '' snip_install_the_eastwest_gateway_in_cluster2_2_out <<\ENDSNIP
-helm install istio-eastwestgateway istio/gateway -n istio-system --kube-context $CTX_CLUSTER2 --set name=istio-eastwestgateway --set networkGateway=network2
+helm install istio-eastwestgateway istio/gateway -n istio-system --kube-context ${CTX_CLUSTER2} --set name=istio-eastwestgateway --set networkGateway=network2
 ENDSNIP
 
 snip_install_the_eastwest_gateway_in_cluster2_3() {
