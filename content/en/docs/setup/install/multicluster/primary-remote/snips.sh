d92c82c6fa0b3d74f8b1bdc812b7fc3328f5c984
nshankar <nshankar@microsoft.com>
2024-07-30 11:36:37 -0400

    Update docs
    Signed-off-by: nshankar <nshankar@microsoft.com>

--- content/en/docs/setup/install/multicluster/primary-remote/snips.sh
+++ content/en/docs/setup/install/multicluster/primary-remote/snips.sh
@@ -39,7 +39,7 @@ istioctl install --set values.pilot.env.EXTERNAL_ISTIOD=true --context="${CTX_CL
 }
 
 snip_configure_cluster1_as_a_primary_3() {
-kubectl create namespace istio-system --kube-context ${CTX_CLUSTER1}
+kubectl create namespace istio-system --context ${CTX_CLUSTER1}
 helm install istio-base istio/base -n istio-system --set global.externalIstiod=true --kube-context ${CTX_CLUSTER1}
 }
 
