0cc21c46aa1c55dd2b245321fea6625855d67173
nshankar <nshankar@microsoft.com>
2024-07-30 14:16:19 -0400

    Fix lints
    Signed-off-by: nshankar <nshankar@microsoft.com>

--- content/en/docs/setup/install/multicluster/multi-primary_multi-network/snips.sh
+++ content/en/docs/setup/install/multicluster/multi-primary_multi-network/snips.sh
@@ -44,7 +44,7 @@ istioctl install --context="${CTX_CLUSTER1}" -f cluster1.yaml
 }
 
 snip_configure_cluster1_as_a_primary_3() {
-helm install istio-base istio/base -n istio-system --kube-context "${CTX_CLUSTER1}" 
+helm install istio-base istio/base -n istio-system --kube-context "${CTX_CLUSTER1}"
 }
 
 snip_configure_cluster1_as_a_primary_4() {
