ac30af39c129af3d4613144937e409fabf92cb7d
Craig Box <craig.box@gmail.com>
2024-08-20 13:23:43 +1200

    fix fix moar tests

--- content/en/docs/ambient/upgrade/helm/common.sh
+++ content/en/docs/ambient/upgrade/helm/common.sh
@@ -12,12 +12,12 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-source "content/en/docs/ambient/install/helm-installation/snips.sh"
+source "content/en/docs/ambient/install/helm/snips.sh"
 
 _install_istio_ambient_helm() {
     snip_configure_helm
     _rewrite_helm_repo snip_install_base
-    _rewrite_helm_repo snip_install_discovery
+    _rewrite_helm_repo snip_install_istiod
     _wait_for_deployment istio-system istiod
     _rewrite_helm_repo snip_install_cni
     _wait_for_daemonset istio-system istio-cni-node
@@ -30,7 +30,7 @@ _install_istio_ambient_helm() {
 _remove_istio_ambient_helm() {
     snip_delete_cni
     snip_delete_ztunnel
-    snip_delete_discovery
+    snip_delete_istiod
     snip_delete_base
     snip_delete_system_namespace
     snip_delete_ingress
