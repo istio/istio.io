2c231cbb0ddc552841c6c7205cd28e57f25047e0
Benjamin Leggett <benjamin.leggett@solo.io>
2024-10-01 19:07:35 -0400

    Fixup again
    Signed-off-by: Benjamin Leggett <benjamin.leggett@solo.io>

--- content/en/boilerplates/snips/crd-upgrade-123.sh
+++ content/en/boilerplates/snips/crd-upgrade-123.sh
@@ -21,7 +21,7 @@
 ####################################################################################################
 
 bpsnip_crd_upgrade_123_install_experimental_crds() {
-kubectl label crds -l chart=istio "app.kubernetes.io/managed-by=Helm"
-kubectl annotate crds -l chart=istio "meta.helm.sh/release-name=istio-base" # replace with actual Helm release name, if different from the documentation default
-kubectl annotate crds -l chart=istio "meta.helm.sh/release-namespace=istio-system" # replace with actual istio namespace
+kubectl label crds -l chart=istio -l app.kubernetes.io/part-of=istio "app.kubernetes.io/managed-by=Helm"
+kubectl annotate crds -l chart=istio -l app.kubernetes.io/part-of=istio "meta.helm.sh/release-name=istio-base" # replace with actual Helm release name, if different from the documentation default
+kubectl annotate crds -l chart=istio -l app.kubernetes.io/part-of=istio "meta.helm.sh/release-namespace=istio-system" # replace with actual istio namespace
 }
