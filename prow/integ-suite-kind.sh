daccb4888ce087c61722bf9495de77ce70f7d6af
Faseela K <faseela.k@est.tech>
2024-11-08 23:18:01 +0100

    update kind node image version and point go.mod to master (#15898)
    * update node image
    
    Signed-off-by: Faseela K <faseela.k@est.tech>
    
    * lint
    
    Signed-off-by: Faseela K <faseela.k@est.tech>
    
    * update default config yaml
    
    Signed-off-by: Faseela K <faseela.k@est.tech>
    
    * Point go.mod to master instead of 1.24
    
    * others too
    
    ---------
    
    Signed-off-by: Faseela K <faseela.k@est.tech>
    Co-authored-by: John Howard <john.howard@solo.io>

--- prow/integ-suite-kind.sh
+++ prow/integ-suite-kind.sh
@@ -105,7 +105,7 @@ if [ -n "${PULL_NUMBER:-}" ]; then
 fi
 
 export IP_FAMILY="${IP_FAMILY:-ipv4}"
-export NODE_IMAGE="gcr.io/istio-testing/kind-node:v1.27.3"
+export NODE_IMAGE="gcr.io/istio-testing/kind-node:v1.31.0"
 
 if [[ -z "${SKIP_SETUP:-}" ]]; then
   export ARTIFACTS="${ARTIFACTS:-$(mktemp -d)}"
