aa68b03162e4dbc2b94c19c95707c623503314b1
nshankar <nshankar@microsoft.com>
2024-11-26 10:37:19 -0500

    Fix cleanup steps
    Signed-off-by: nshankar <nshankar@microsoft.com>

--- content/en/docs/setup/install/multicluster/multi-primary/helm_test.sh
+++ content/en/docs/setup/install/multicluster/multi-primary/helm_test.sh
@@ -50,11 +50,7 @@ function enable_endpoint_discovery {
   snip_enable_endpoint_discovery_2
 }
 
-function delete_crds_cluster_1() {
-  kubectl get crd -oname --context "${CTX_CLUSTER1}" | grep --color=never 'istio.io' | xargs kubectl delete --context "${CTX_CLUSTER1}"
-}
-
-time delete_crds_cluster_1
+snip_delete_crds
 
 time configure_trust
 time install_istio_helm
