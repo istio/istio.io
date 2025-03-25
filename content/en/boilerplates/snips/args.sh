1d92ed2d60c3c530aea9387ee8e5e1397874eede
Daniel Hawton <daniel@hawton.org>
2025-03-25 06:29:19 -0600

    add changelog for 1.25.1
    Signed-off-by: Daniel Hawton <daniel@hawton.org>

--- content/en/boilerplates/snips/args.sh
+++ content/en/boilerplates/snips/args.sh
@@ -29,5 +29,5 @@ ENDSNIP
 ENDSNIP
 
 ! IFS=$'\n' read -r -d '' bpsnip_args_istio_full_version <<\ENDSNIP
-1.25.0
+1.25.1
 ENDSNIP
--- content/en/docs/setup/upgrade/canary/snips.sh
+++ content/en/docs/setup/upgrade/canary/snips.sh
@@ -99,12 +99,12 @@ istioctl proxy-status | grep "\.test-ns "
 
 snip_usage_1() {
 istioctl install --revision=1-24-1 --set profile=minimal --skip-confirmation
-istioctl install --revision=1-25-0 --set profile=minimal --skip-confirmation
+istioctl install --revision=1-25-1 --set profile=minimal --skip-confirmation
 }
 
 snip_usage_2() {
 istioctl tag set prod-stable --revision 1-24-1
-istioctl tag set prod-canary --revision 1-25-0
+istioctl tag set prod-canary --revision 1-25-1
 }
 
 snip_usage_3() {
@@ -128,13 +128,13 @@ istioctl ps
 
 ! IFS=$'\n' read -r -d '' snip_usage_5_out <<\ENDSNIP
 NAME                                CLUSTER        CDS        LDS        EDS        RDS        ECDS         ISTIOD                             VERSION
-curl-78ff5975c6-62pzf.app-ns-3      Kubernetes     SYNCED     SYNCED     SYNCED     SYNCED     NOT SENT     istiod-1-25-0-7f6fc6cfd6-s8zfg     1.25.0
+curl-78ff5975c6-62pzf.app-ns-3      Kubernetes     SYNCED     SYNCED     SYNCED     SYNCED     NOT SENT     istiod-1-25-1-7f6fc6cfd6-s8zfg     1.25.1
 curl-78ff5975c6-8kxpl.app-ns-1      Kubernetes     SYNCED     SYNCED     SYNCED     SYNCED     NOT SENT     istiod-1-24-1-bdf5948d5-n72r2      1.24.1
 curl-78ff5975c6-8q7m6.app-ns-2      Kubernetes     SYNCED     SYNCED     SYNCED     SYNCED     NOT SENT     istiod-1-24-1-bdf5948d5-n72r2      1-24.1
 ENDSNIP
 
 snip_usage_6() {
-istioctl tag set prod-stable --revision 1-25-0 --overwrite
+istioctl tag set prod-stable --revision 1-25-1 --overwrite
 }
 
 snip_usage_7() {
@@ -148,13 +148,13 @@ istioctl ps
 
 ! IFS=$'\n' read -r -d '' snip_usage_8_out <<\ENDSNIP
 NAME                                                   CLUSTER        CDS        LDS        EDS        RDS          ECDS         ISTIOD                             VERSION
-curl-5984f48bc7-kmj6x.app-ns-1                         Kubernetes     SYNCED     SYNCED     SYNCED     SYNCED       NOT SENT     istiod-1-25-0-7f6fc6cfd6-jsktb     1.25.0
-curl-78ff5975c6-jldk4.app-ns-3                         Kubernetes     SYNCED     SYNCED     SYNCED     SYNCED       NOT SENT     istiod-1-25-0-7f6fc6cfd6-jsktb     1.25.0
-curl-7cdd8dccb9-5bq5n.app-ns-2                         Kubernetes     SYNCED     SYNCED     SYNCED     SYNCED       NOT SENT     istiod-1-25-0-7f6fc6cfd6-jsktb     1.25.0
+curl-5984f48bc7-kmj6x.app-ns-1                         Kubernetes     SYNCED     SYNCED     SYNCED     SYNCED       NOT SENT     istiod-1-25-1-7f6fc6cfd6-jsktb     1.25.1
+curl-78ff5975c6-jldk4.app-ns-3                         Kubernetes     SYNCED     SYNCED     SYNCED     SYNCED       NOT SENT     istiod-1-25-1-7f6fc6cfd6-jsktb     1.25.1
+curl-7cdd8dccb9-5bq5n.app-ns-2                         Kubernetes     SYNCED     SYNCED     SYNCED     SYNCED       NOT SENT     istiod-1-25-1-7f6fc6cfd6-jsktb     1.25.1
 ENDSNIP
 
 snip_default_tag_1() {
-istioctl tag set default --revision 1-25-0
+istioctl tag set default --revision 1-25-1
 }
 
 snip_uninstall_old_control_plane_1() {
