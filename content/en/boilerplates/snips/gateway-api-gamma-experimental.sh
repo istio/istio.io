2fcd8ca488476b7aecd8e86f2cd578ed0a73c393
istio-testing <istio-testing-bot@google.com>
2025-04-13 02:04:02 +0000

    Automator: update istio@ test reference

--- content/en/boilerplates/snips/gateway-api-gamma-experimental.sh
+++ content/en/boilerplates/snips/gateway-api-gamma-experimental.sh
@@ -21,7 +21,7 @@
 ####################################################################################################
 
 bpsnip_gateway_api_gamma_experimental_install_experimental_crds() {
-kubectl kustomize "github.com/kubernetes-sigs/gateway-api/config/crd/experimental?ref=v1.2.1" | kubectl apply -f -
+kubectl kustomize "github.com/kubernetes-sigs/gateway-api/config/crd/experimental?ref=92efbedcc2b40dc097b7ea0eacb894a6033057e1" | kubectl apply -f -
 }
 
 bpsnip_gateway_api_gamma_experimental_enable_alpha_crds() {
