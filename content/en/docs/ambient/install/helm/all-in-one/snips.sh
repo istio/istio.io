71f581d3618ea296d550399b528ab59ddd6f5503
Gustavo Meira <grnmeira@users.noreply.github.com>
2025-06-20 18:17:10 +0100

    release note for 1.26.2 (#16587)
    * release note for 1.26.2
    
    * make gen
    
    * fixing linter error
    
    * Update content/en/news/releases/1.26.x/announcing-1.26.2/index.md
    
    Co-authored-by: Craig Box <craig.box@gmail.com>
    
    * Update content/en/news/releases/1.26.x/announcing-1.26.2/index.md
    
    Co-authored-by: Craig Box <craig.box@gmail.com>
    
    * Update index.md with probable release date
    
    * Update index.md with release date
    
    ---------
    
    Co-authored-by: Craig Box <craig.box@gmail.com>

--- content/en/docs/ambient/install/helm/all-in-one/snips.sh
+++ content/en/docs/ambient/install/helm/all-in-one/snips.sh
@@ -44,7 +44,7 @@ helm ls -n istio-system
 
 ! IFS=$'\n' read -r -d '' snip_show_components_out <<\ENDSNIP
 NAME            NAMESPACE       REVISION    UPDATED                                 STATUS      CHART           APP VERSION
-istio-ambient      istio-system    1           2024-04-17 22:14:45.964722028 +0000 UTC deployed    ambient-1.26.1     1.26.1
+istio-ambient      istio-system    1           2024-04-17 22:14:45.964722028 +0000 UTC deployed    ambient-1.26.2     1.26.2
 ENDSNIP
 
 snip_check_pods() {
