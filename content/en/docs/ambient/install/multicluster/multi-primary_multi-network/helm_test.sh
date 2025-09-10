d01ce2075990ad62bea12b468b7eb8efcc997a50
Faseela K <faseela.k@est.tech>
2025-09-10 12:56:27 +0200

    fix ambient multi cluster
    Signed-off-by: Faseela K <faseela.k@est.tech>

--- content/en/docs/ambient/install/multicluster/multi-primary_multi-network/helm_test.sh
+++ content/en/docs/ambient/install/multicluster/multi-primary_multi-network/helm_test.sh
@@ -21,8 +21,8 @@ set -e
 set -u
 set -o pipefail
 
-source content/en/docs/ambient/install/multicluster/common.sh
 source "tests/util/gateway-api.sh"
+source "content/en/docs/ambient/install/multicluster/common.sh"
 
 set_multi_network_vars
 setup_helm_repo
@@ -82,14 +82,14 @@ function enable_endpoint_discovery {
   snip_enable_endpoint_discovery_2
 }
 
-
+#shellcheck source=content/en/docs/setup/install/multicluster/common.sh
 time configure_trust
 time install_istio_helm
 time enable_endpoint_discovery
 time verify_load_balancing
 
 # @cleanup
-source content/en/docs/setup/install/multicluster/common.sh
+source "content/en/docs/setup/install/multicluster/common.sh"
 set_multi_network_vars
 
 function cleanup_cluster1_helm {
--- content/en/docs/setup/install/multicluster/multi-primary/helm_test.sh
+++ content/en/docs/setup/install/multicluster/multi-primary/helm_test.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# shellcheck disable=SC1090,SC2154 source=content/en/docs/setup/install/multicluster/common.sh
+# shellcheck disable=SC1090,SC2154
 
 # Copyright Istio Authors
 #
