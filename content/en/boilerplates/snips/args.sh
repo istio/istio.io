cdf57757371b20e91ec5b6bdd534fea8a6a57940
Gustavo <grnmeira@gmail.com>
2026-01-19 13:06:50 +0000

    make gen

--- content/en/boilerplates/snips/args.sh
+++ content/en/boilerplates/snips/args.sh
@@ -25,9 +25,9 @@ v1.4.0
 ENDSNIP
 
 ! IFS=$'\n' read -r -d '' bpsnip_args_istio_previous_version <<\ENDSNIP
-1.28
+1.27
 ENDSNIP
 
 ! IFS=$'\n' read -r -d '' bpsnip_args_istio_full_version <<\ENDSNIP
-1.29.0
+1.28.3
 ENDSNIP
