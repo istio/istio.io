7368b3621651ee695503f9d764d752266f01f62e
Jacek Ewertowski <jacek.ewertowski1@gmail.com>
2026-08-31 13:14:17 +0200

    update data/versions.yml and archive v1.30

---
+++ archive/v1.30/docs/tasks/observability/distributed-tracing/zipkin/snips.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+# shellcheck disable=SC2034,SC2153,SC2155,SC2164
+
+# Copyright Istio Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+####################################################################################################
+# WARNING: THIS IS AN AUTO-GENERATED FILE, DO NOT EDIT. PLEASE MODIFY THE ORIGINAL MARKDOWN FILE:
+#          docs/tasks/observability/distributed-tracing/zipkin/index.md
+####################################################################################################
+source "content/en/boilerplates/snips/trace-generation.sh"
+
+snip_configure_an_extension_provider_1() {
+cat <<EOF > ./tracing.yaml
+apiVersion: install.istio.io/v1alpha1
+kind: IstioOperator
+spec:
+  meshConfig:
+    enableTracing: true
+    defaultConfig:
+      tracing: {} # disable legacy MeshConfig tracing options
+    extensionProviders:
+    - name: zipkin
+      zipkin:
+        service: zipkin.istio-system.svc.cluster.local
+        port: 9411
+EOF
+istioctl install -f ./tracing.yaml --skip-confirmation
+}
+
+snip_enable_tracing_1() {
+kubectl apply -f - <<EOF
+apiVersion: telemetry.istio.io/v1
+kind: Telemetry
+metadata:
+  name: mesh-default
+  namespace: istio-system
+spec:
+  tracing:
+  - providers:
+    - name: zipkin
+EOF
+}
+
+snip_accessing_the_dashboard_1() {
+istioctl dashboard zipkin
+}
+
+snip_cleanup_1() {
+killall istioctl
+}
